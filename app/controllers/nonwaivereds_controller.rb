7869220bb39a8cfb28f83faff80c9d75864568e6
Jake <github@jakewendt.com>
2011-06-01 16:57:39 -0700

    Added comment in prep for #66

--- app/controllers/nonwaivereds_controller.rb
+++ app/controllers/nonwaivereds_controller.rb
@@ -10,6 +10,15 @@ class NonwaiveredsController < ApplicationController
 #	deep_merge does not work correctly with a HashWithIndifferentAccess
 #	convert to hash, but MUST use string keys, not symbols as
 #		real request do not send symbols
+
+#	TODO	#66
+#
+#	If a consent date has been supplied, 
+#		set consented equal to true, yes, 1 or whatever is right for the data type.
+#
+#	If no consent date has been supplied, 
+#		leave the consented field null too. 
+#
 		@subject = Subject.new(params[:subject].to_hash.deep_merge({
 			'subject_type_id' => SubjectType['Case'].id,
 			'identifier_attributes' => {
