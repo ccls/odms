9059eae91dbab3353c43eb70444a2da2865f4704
Jake <github@jakewendt.com>
2011-06-08 15:10:56 -0700

    Prepping for possible ajax zip code access.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -78,6 +78,58 @@ jQuery(function(){
 		}
 	});
 
+/*
+
+
+
+	NOTE that there are now 2 "change" event calls for this element.
+		it will probably cause some grief, so we'll need to figure this out.
+
+
+
+*/
+
+	jQuery('#subject_patient_attributes_raf_zip').change(function(){
+		jQuery.get(root + '/zip_codes.json?q=' + jQuery(this).val(), function(data){
+//[{"zip_code":{"county_name":"Schenectady","city":"SCHENECTADY","zip_code":"12345","state":"NY"}}]
+			if(data.length == 1) {
+				var raf_zip = jQuery('#subject_patient_attributes_raf_zip');
+				if( raf_zip ){
+					raf_zip.val(data[0].zip_code.zip_code);
+				}
+//	zip_codes outside of CA aren't listed
+				var raf_county_id = jQuery('#subject_patient_attributes_raf_county_id');
+				if( raf_county_id ){
+					raf_county_id.val(data[0].zip_code.county_id);
+				}
+			}
+		});
+	});
+
+	jQuery('#subject_addressings_attributes_0_address_attributes_zip').change(function(){
+		jQuery.get(root + '/zip_codes.json?q=' + jQuery(this).val(), function(data){
+//[{"zip_code":{"county_name":"Schenectady","city":"SCHENECTADY","zip_code":"12345","state":"NY"}}]
+			if(data.length == 1) {
+				var address_zip = jQuery('#subject_addressings_attributes_0_address_attributes_zip');
+				if( address_zip ){
+					address_zip.val(data[0].zip_code.zip_code);
+				}
+				var address_county = jQuery('#subject_addressings_attributes_0_address_attributes_county');
+				if( address_county && !address_county.val() ){
+					address_county.val(data[0].zip_code.county_name);
+				}
+				var address_city = jQuery('#subject_addressings_attributes_0_address_attributes_city');
+				if( address_city && !address_city.val() ){
+					address_city.val(data[0].zip_code.city);
+				}
+				var address_state = jQuery('#subject_addressings_attributes_0_address_attributes_state');
+				if( address_state && !address_state.val() ){
+					address_state.val(data[0].zip_code.state);
+				}
+			}
+		});
+	});
+
 });
 
 var submit_form = function() {
