c33a373932cd1dcd5ddd97d59942818386fba473
Jake <github@jakewendt.com>
2011-10-06 11:56:06 -0700

    Removed old code.

--- app/helpers/study_subjects_helper.rb
+++ app/helpers/study_subjects_helper.rb
@@ -1,39 +1,5 @@
 module StudySubjectsHelper
 
-#	TODO move much of this into ccls_engine as is used both in ODMS and HOMEX
-#		ccls_engine/lib/ccls_engine/helper.rb
-
-#	#	Used to replace the _id_bar partial
-#	def study_subject_id_bar(study_subject,&block)		#	TODO remove as added to ccls_engine > 3.8.7
-#		stylesheets('study_subject_id_bar')
-#		content_for :main do
-#			"<div id='id_bar'>\n" <<
-#			"<div class='childid'>\n" <<
-#			"<span>ChildID:</span>\n" <<
-#			"<span>#{study_subject.try(:childid)}</span>\n" <<
-#			"</div><!-- class='childid' -->\n" <<
-#			"<div class='studyid'>\n" <<
-#			"<span>StudyID:</span>\n" <<
-#			"<span>#{study_subject.try(:studyid)}</span>\n" <<
-#			"</div><!-- class='studyid' -->\n" <<
-#			"<div class='full_name'>\n" <<
-#			"<span>#{study_subject.try(:full_name)}</span>\n" <<
-#			"</div><!-- class='full_name' -->\n" <<
-#			"<div class='controls'>\n" <<
-#			@content_for_id_bar.to_s <<
-#			((block_given?)?yield: '') <<
-#			"</div><!-- class='controls' -->\n" <<
-#			"</div><!-- id='id_bar' -->\n"
-#		end
-#
-#		content_for :main do
-#			"<div id='do_not_contact'>\n" <<
-#			"Subject requests no further contact with Study.\n" <<
-#			"</div>\n" 
-#		end if study_subject.try(:do_not_contact?)
-#	end	#	id_bar_for
-
-
 #	Move this down with the subject_languages_select'r
 	def select_subject_races(study_subject)
 		#	NOTE THAT THIS PREFIX HAS ONLY THE FIRST LEFT SIDE [
