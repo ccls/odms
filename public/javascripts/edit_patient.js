cfef8d8d9e358220f99b46d914813fec45c8fbb6
Jake <github@jakewendt.com>
2011-10-21 10:24:38 -0700

    Styling updates.

--- public/javascripts/edit_patient.js
+++ public/javascripts/edit_patient.js
@@ -1,3 +1,4 @@
+/*
 var initial_diagnosis_date;
 function diagnosis_date(){
 	return jQuery('#patient_diagnosis_date').val();
@@ -14,6 +15,27 @@ jQuery(function(){
 			$(this).parent().addClass('changed');
 		} else {
 			$(this).parent().removeClass('changed');
+		}
+	});
+
+});
+*/
+var initial_admit_date;
+function admit_date(){
+	return jQuery('#patient_admit_date').val();
+}
+function admit_date_changed(){
+	return initial_admit_date != admit_date();
+}
+jQuery(function(){
+
+	initial_admit_date = admit_date();
+	
+	jQuery('#patient_admit_date').change(function(){
+		if( admit_date_changed() ) {
+			$(this).parent().parent().addClass('changed');
+		} else {
+			$(this).parent().parent().removeClass('changed');
 		}
 	});
 
