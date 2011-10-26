6816d3739b1f68569b6b81a3f2b102f66663df3e
Jake <github@jakewendt.com>
2011-10-26 14:21:54 -0700

    Nearly all of subject finder implmented.[#28]

--- app/controllers/study_subjects_controller.rb
+++ app/controllers/study_subjects_controller.rb
@@ -14,9 +14,61 @@ class StudySubjectsController < ApplicationController
 
 	def find
 		record_or_recall_sort_order
-		@study_subjects = StudySubject.search(params)
+		operator = ' OR '
+		if params[:operator] and !params[:operator].blank? and 
+				['AND','OR'].include?(params[:operator])
+			operator = " #{params[:operator]} "
+		end
+		conditions = [[],[]]
+		if params[:first_name] and !params[:first_name].blank?
+			conditions[0] << "( piis.first_name LIKE ? )"
+			conditions[1] << "%#{params[:first_name]}%"
+		end
+		if params[:last_name] and !params[:last_name].blank?
+			conditions[0] << "( piis.last_name LIKE ? )"
+			conditions[1] << "%#{params[:last_name]}%"
+		end
+		if params[:childid] and !params[:childid].blank?
+			conditions[0] << "( identifiers.childid LIKE ? )"
+			conditions[1] << "%#{params[:childid]}%"
+		end
+		if params[:patid] and !params[:patid].blank?
+			conditions[0] << "( identifiers.patid LIKE ? )"
+			conditions[1] << "%#{params[:patid]}%"
+		end
+		if params[:hospital_no] and !params[:hospital_no].blank?
+			conditions[0] << "( identifiers.hospital_no LIKE ? )"
+			conditions[1] << "%#{params[:hospital_no]}%"
+		end
+		if params[:icf_master_id] and !params[:icf_master_id].blank?
+			conditions[0] << "( identifiers.icf_master_id LIKE ? )"
+			conditions[1] << "%#{params[:icf_master_id]}%"
+		end
+		if params[:dob] and !params[:dob].blank?
+#			conditions[0] << "( identifiers.icf_master_id LIKE ? )"
+#			conditions[1] << "%#{params[:dob]}%"
+		end
+		if params[:registrar_no] and !params[:registrar_no].blank?
+			conditions[0] << "( identifiers.state_id_no LIKE ? OR identifiers.state_registrar_no LIKE ? OR identifiers.local_registrar_no LIKE ? )"
+			conditions[1] << "%#{params[:registrar_no]}%"
+			conditions[1] << "%#{params[:registrar_no]}%"
+			conditions[1] << "%#{params[:registrar_no]}%"
+		end
+		@study_subjects = StudySubject.paginate(
+			:include => [:pii,:patient,:identifier],
+			:joins => [
+				'LEFT JOIN piis ON study_subjects.id = piis.study_subject_id',
+				'LEFT JOIN patients ON study_subjects.id = patients.study_subject_id',
+				'LEFT JOIN identifiers ON study_subjects.id = identifiers.study_subject_id',
+			],
+			:conditions => [ conditions[0].join(operator),
+					*(conditions[1].flatten) ],
+			:per_page => params[:per_page]||25,
+			:page     => params[:page]||1
+		)
 	end
 
+	#	there is no longer a link to this action, nevertheless
 	def index
 		record_or_recall_sort_order
 		if params[:commit] && params[:commit] == 'download'
