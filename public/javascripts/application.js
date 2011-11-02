7e52683e08da85a634909fee971de723077efdf8
Jake <github@jakewendt.com>
2011-11-02 13:03:37 -0700

    Removed unused javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -18,22 +18,6 @@ jQuery(function(){
 		return false;
 	});
 
-/*
-	jQuery('form.destroy_link_to').submit(function(){
-		var message = "Destroy?  Seriously?"
-		if( this.confirm && this.confirm.value ) {
-			message = this.confirm.value
-		}
-		if( !confirm(message) ){
-			return false;
-		}
-	});
-
-	jQuery('p.flash').click(function(){jQuery(this).remove();});
-
-	jQuery('.datepicker').datepicker();
-*/
-
 	jQuery('a.ajax').click(function(){
 		jQuery.get(jQuery(this).attr('href')+'.js', function(data){
 			jQuery('#ajax').html(data);
