e7b2a530b952eb92bbbfb21bdfd12d8df8d63164
Jake <github@jakewendt.com>
2011-11-04 11:14:30 -0700

    Updated phone number form.[#79]

--- public/javascripts/edit_phone_number.js
+++ public/javascripts/edit_phone_number.js
@@ -1,5 +1,6 @@
 jQuery(function(){
 
+/*
 	jQuery('.valid .is_valid #phone_number_is_valid').change(function(){
 		toggle_why_invalid( ( $(this).val()!=2 && $(this).val()!=999 ) );
 	});
@@ -14,6 +15,24 @@ jQuery(function(){
 
 	toggle_how_verified(
 		$('.verified .is_verified #phone_number_is_verified').attr('checked'));
+
+	removing what I hope are unnecessary css qualifiers
+*/
+
+	jQuery('#phone_number_is_valid').change(function(){
+		toggle_why_invalid( ( $(this).val()!=2 && $(this).val()!=999 ) );
+	});
+
+	jQuery('#phone_number_is_verified').change(function(){
+		toggle_how_verified($(this).attr('checked'));
+	});
+
+	toggle_why_invalid( 
+		( $('#phone_number_is_valid').val()!=2 && 
+			$('#phone_number_is_valid').val()!=999 ) );
+
+	toggle_how_verified(
+		$('#phone_number_is_verified').attr('checked'));
 
 });
 /*
