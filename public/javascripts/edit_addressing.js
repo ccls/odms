3778012e7086703dea9f6673a6144d9d44f5950b
Jake <github@jakewendt.com>
2011-11-04 11:49:59 -0700

    Updated addressing form.[#99]

--- public/javascripts/edit_addressing.js
+++ public/javascripts/edit_addressing.js
@@ -1,19 +1,25 @@
 jQuery(function(){
 
-	jQuery('.valid .is_valid #addressing_is_valid').change(function(){
+	jQuery('#addressing_is_valid').change(function(){
 		toggle_why_invalid( ( $(this).val()!=2 && $(this).val()!=999 ) );
 	});
 
-	jQuery('.verified .is_verified #addressing_is_verified').change(function(){
+  toggle_why_invalid( 
+		( $('#addressing_is_valid').val()!=2 && 
+			$('#addressing_is_valid').val()!=999 ) );
+
+	jQuery('#addressing_is_verified').change(function(){
 		toggle_how_verified($(this).attr('checked'));
 	});
 
-  toggle_why_invalid( 
-		( $('.valid .is_valid #addressing_is_valid').val()!=2 && 
-			$('.valid .is_valid #addressing_is_valid').val()!=999 ) );
-
   toggle_how_verified( 
-		$('.verified .is_verified #addressing_is_verified').attr('checked') );
+		$('#addressing_is_verified').attr('checked') );
+
+/*
+
+	toggle data source other
+
+*/
 
 });
 /*
--- public/javascripts/edit_phone_number.js
+++ public/javascripts/edit_phone_number.js
@@ -1,38 +1,27 @@
 jQuery(function(){
 
-/*
-	jQuery('.valid .is_valid #phone_number_is_valid').change(function(){
-		toggle_why_invalid( ( $(this).val()!=2 && $(this).val()!=999 ) );
-	});
-
-	jQuery('.verified .is_verified #phone_number_is_verified').change(function(){
-		toggle_how_verified($(this).attr('checked'));
-	});
-
-	toggle_why_invalid( 
-		( $('.valid .is_valid #phone_number_is_valid').val()!=2 && 
-			$('.valid .is_valid #phone_number_is_valid').val()!=999 ) );
-
-	toggle_how_verified(
-		$('.verified .is_verified #phone_number_is_verified').attr('checked'));
-
-	removing what I hope are unnecessary css qualifiers
-*/
-
 	jQuery('#phone_number_is_valid').change(function(){
 		toggle_why_invalid( ( $(this).val()!=2 && $(this).val()!=999 ) );
-	});
-
-	jQuery('#phone_number_is_verified').change(function(){
-		toggle_how_verified($(this).attr('checked'));
 	});
 
 	toggle_why_invalid( 
 		( $('#phone_number_is_valid').val()!=2 && 
 			$('#phone_number_is_valid').val()!=999 ) );
 
+	jQuery('#phone_number_is_verified').change(function(){
+		toggle_how_verified($(this).attr('checked'));
+	});
+
 	toggle_how_verified(
 		$('#phone_number_is_verified').attr('checked'));
+
+/*
+
+
+	toggle data source other
+
+
+*/
 
 });
 /*
