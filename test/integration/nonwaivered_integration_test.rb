a502c1a0bed8bf868bc15e32b41a0d627c5aba13
Jake <github@jakewendt.com>
2011-11-13 18:45:39 -0800

    Added duplicate tests.

--- test/integration/nonwaivered_integration_test.rb
+++ test/integration/nonwaivered_integration_test.rb
@@ -4,10 +4,116 @@ class NonwaiveredIntegrationTest < ActionController::IntegrationTest
 
 	site_administrators.each do |cu|
 
-		test "should add a couple duplicate subject tests here with #{cu} login" do
-pending	#	TODO	complete_nonwaivered_case_study_subject
-#puts Factory.attributes_for(:complete_case_study_subject).inspect
-#{:pii_attributes=>{:dob=>Tue, 30 Dec 2003, :email=>"email2@example.com"}, :patient_attributes=>{:admit_date=>Sun, 13 Nov 2011, :hospital_no=>"2", :organization_id=>1, :diagnosis_id=>1}, :subject_type=>#<SubjectType id: 1, position: nil, code: "Case", description: "Case", related_case_control_type: nil, created_at: "2011-11-13 20:43:35", updated_at: "2011-11-13 20:43:35">, :sex=>"M", :identifier_attributes=>{:state_registrar_no=>"2", :local_registrar_no=>"2", :icf_master_id=>"2", :gbid=>"2", :accession_no=>"2", :ssn=>"000000002", :lab_no_wiemels=>"2", :state_id_no=>"2", :idno_wiemels=>"2", :case_control_type=>"c"}}
+		test "should NOT create subject if duplicate subject match found with #{cu} login" do
+			duplicate = Factory(:complete_nonwaivered_case_study_subject)
+			login_as send(cu)
+			visit new_nonwaivered_path
+
+			subject = Factory.build(:complete_nonwaivered_case_study_subject)	#	build, but DON'T save
+			#	by using the attributes from this built subject, we test the factory and use its sequencing
+
+			select "male", 	#	text NOT the value
+				:from => "study_subject[sex]"
+			fill_in "study_subject[patient_attributes][hospital_no]",
+				:with => duplicate.hospital_no						#	should trigger duplicate found
+			select subject.organization.to_s,
+				:from => "study_subject[patient_attributes][organization_id]"
+			fill_in "study_subject[patient_attributes][admit_date]",
+				:with => subject.admit_date.strftime("%m/%d/%Y")
+			select "AML", 
+				:from => "study_subject[patient_attributes][diagnosis_id]"
+			fill_in "study_subject[pii_attributes][dob]",
+				:with => subject.dob.strftime("%m/%d/%Y")
+
+			assert_difference('PhoneNumber.count',0) {
+			assert_difference('Addressing.count',0) {
+			assert_difference('Address.count',0) {
+			assert_difference('Pii.count',0) {
+			assert_difference('Patient.count',0) {
+			assert_difference('Identifier.count',0) {
+			assert_difference('Enrollment.count',0) {
+			assert_difference('StudySubject.count',0) {
+				#	click_button(value)
+				click_button "Submit"	
+			} } } } } } } }
+
+			#	is not redirected, is rendered, therefore it is just a path, not a full url
+			assert_equal nonwaivered_path, current_url
+			assert_not_nil flash[:error]	#	Possible Duplicate(s) Found.
+			assert_match /Possible Duplicate\(s\) Found/, flash[:error]
+
+			choose "duplicate_id_#{duplicate.id}"
+			assert_difference('PhoneNumber.count',0) {
+			assert_difference('Addressing.count',0) {
+			assert_difference('Address.count',0) {
+			assert_difference('Pii.count',0) {
+			assert_difference('Patient.count',0) {
+			assert_difference('Identifier.count',0) {
+			assert_difference('Enrollment.count',0) {
+			assert_difference('StudySubject.count',0) {
+			assert_difference('OperationalEvent.count',1) {
+				#	click_button(value)
+				click_button "Match Found"	
+			} } } } } } } } }
+
+			assert_not_nil flash[:notice]
+			assert_match /Operational Event created marking this attempted entry/, flash[:notice]
+			assert_equal study_subject_url( duplicate ), current_url
+		end
+
+		test "should create subject if duplicate subject no match found with #{cu} login" do
+			duplicate = Factory(:complete_nonwaivered_case_study_subject)
+			login_as send(cu)
+			visit new_nonwaivered_path
+
+			subject = Factory.build(:complete_nonwaivered_case_study_subject)	#	build, but DON'T save
+			#	by using the attributes from this built subject, we test the factory and use its sequencing
+
+			select "male", 	#	text NOT the value
+				:from => "study_subject[sex]"
+			fill_in "study_subject[patient_attributes][hospital_no]",
+				:with => duplicate.hospital_no						#	should trigger duplicate found
+			select subject.organization.to_s,
+				:from => "study_subject[patient_attributes][organization_id]"
+			fill_in "study_subject[patient_attributes][admit_date]",
+				:with => subject.admit_date.strftime("%m/%d/%Y")
+			select "AML", 
+				:from => "study_subject[patient_attributes][diagnosis_id]"
+			fill_in "study_subject[pii_attributes][dob]",
+				:with => subject.dob.strftime("%m/%d/%Y")
+
+			assert_difference('PhoneNumber.count',0) {
+			assert_difference('Addressing.count',0) {
+			assert_difference('Address.count',0) {
+			assert_difference('Pii.count',0) {
+			assert_difference('Patient.count',0) {
+			assert_difference('Identifier.count',0) {
+			assert_difference('Enrollment.count',0) {
+			assert_difference('StudySubject.count',0) {
+				#	click_button(value)
+				click_button "Submit"	
+			} } } } } } } }
+
+			#	is not redirected, is rendered, therefore it is just a path, not a full url
+			assert_equal nonwaivered_path, current_url
+			assert_not_nil flash[:error]	#	Possible Duplicate(s) Found.
+			assert_match /Possible Duplicate\(s\) Found/, flash[:error]
+
+			assert_difference('PhoneNumber.count',0) {
+			assert_difference('Addressing.count',0) {
+			assert_difference('Address.count',0) {
+			assert_difference('Pii.count',2) {
+			assert_difference('Patient.count',1) {
+			assert_difference('Identifier.count',2) {
+			assert_difference('Enrollment.count',2) {
+			assert_difference('StudySubject.count',2) {
+				#	click_button(value)
+				click_button "No Match"	
+			} } } } } } } }
+
+#			assert_not_nil flash[:notice]
+#			assert_match /Operational Event created marking this attempted entry/, flash[:notice]
+			assert_equal study_subject_url( assigns(:study_subject) ), current_url
 		end
 
 		test "should get new nonwaivered raf form and submit with #{cu} login" do
@@ -15,21 +121,25 @@ pending	#	TODO	complete_nonwaivered_case_study_subject
 
 			visit new_nonwaivered_path
 
-#	attributes = Factory.attributes_for(:complete_nonwaivered_case_study_subject)
-#	then try to use these attributes in form?
+			subject = Factory.build(:complete_nonwaivered_case_study_subject)	#	build, but DON'T save
+			#	by using the attributes from this built subject, we test the factory and use its sequencing
 
-			select "male", 
+			select "male", 	#	text NOT the value
 				:from => "study_subject[sex]"
 			fill_in "study_subject[patient_attributes][hospital_no]",
-				:with => "9999999999999999999999999"				#	This will NEED to be unique
-			select Hospital.nonwaivered.first.organization.to_s,
+				:with => subject.hospital_no
+#				:with => "9999999999999999999999999"				#	This will NEED to be unique
+#			select Hospital.nonwaivered.first.organization.to_s,
+			select subject.organization.to_s,
 				:from => "study_subject[patient_attributes][organization_id]"
 			fill_in "study_subject[patient_attributes][admit_date]",
-				:with => "1/31/1973"
+				:with => subject.admit_date.strftime("%m/%d/%Y")
+#				:with => "1/31/1973"
 			select "AML", 
 				:from => "study_subject[patient_attributes][diagnosis_id]"
 			fill_in "study_subject[pii_attributes][dob]",
-				:with => "12/5/1971"
+				:with => subject.dob.strftime("%m/%d/%Y")
+#				:with => "12/5/1971"
 
 			assert_difference('PhoneNumber.count',0) {
 			assert_difference('Addressing.count',0) {
@@ -42,6 +152,8 @@ pending	#	TODO	complete_nonwaivered_case_study_subject
 				#	click_button(value)
 				click_button "Submit"	
 			} } } } } } } }
+			assert_nil flash[:error]
+			assert_equal study_subject_url( assigns(:study_subject) ), current_url		#	i would prefer a current_path, but doesn't exist
 		end
 
 	end
--- test/integration/waivered_integration_test.rb
+++ test/integration/waivered_integration_test.rb
@@ -4,8 +4,116 @@ class WaiveredIntegrationTest < ActionController::IntegrationTest
 
 	site_administrators.each do |cu|
 
-		test "should add a couple duplicate subject tests here with #{cu} login" do
-pending	#	TODO	complete_waivered_case_study_subject
+		test "should NOT create subject if duplicate subject match found with #{cu} login" do
+			duplicate = Factory(:complete_waivered_case_study_subject)
+			login_as send(cu)
+			visit new_waivered_path
+
+			subject = Factory.build(:complete_waivered_case_study_subject)	#	build, but DON'T save
+			#	by using the attributes from this built subject, we test the factory and use its sequencing
+
+			select "male", 	#	text NOT the value
+				:from => "study_subject[sex]"
+			fill_in "study_subject[patient_attributes][hospital_no]",
+				:with => duplicate.hospital_no						#	should trigger duplicate found
+			select subject.organization.to_s,
+				:from => "study_subject[patient_attributes][organization_id]"
+			fill_in "study_subject[patient_attributes][admit_date]",
+				:with => subject.admit_date.strftime("%m/%d/%Y")
+			select "AML", 
+				:from => "study_subject[patient_attributes][diagnosis_id]"
+			fill_in "study_subject[pii_attributes][dob]",
+				:with => subject.dob.strftime("%m/%d/%Y")
+
+			assert_difference('PhoneNumber.count',0) {
+			assert_difference('Addressing.count',0) {
+			assert_difference('Address.count',0) {
+			assert_difference('Pii.count',0) {
+			assert_difference('Patient.count',0) {
+			assert_difference('Identifier.count',0) {
+			assert_difference('Enrollment.count',0) {
+			assert_difference('StudySubject.count',0) {
+				#	click_button(value)
+				click_button "Submit"	
+			} } } } } } } }
+
+			#	is not redirected, is rendered, therefore it is just a path, not a full url
+			assert_equal waivered_path, current_url
+			assert_not_nil flash[:error]	#	Possible Duplicate(s) Found.
+			assert_match /Possible Duplicate\(s\) Found/, flash[:error]
+
+			choose "duplicate_id_#{duplicate.id}"
+			assert_difference('PhoneNumber.count',0) {
+			assert_difference('Addressing.count',0) {
+			assert_difference('Address.count',0) {
+			assert_difference('Pii.count',0) {
+			assert_difference('Patient.count',0) {
+			assert_difference('Identifier.count',0) {
+			assert_difference('Enrollment.count',0) {
+			assert_difference('StudySubject.count',0) {
+			assert_difference('OperationalEvent.count',1) {
+				#	click_button(value)
+				click_button "Match Found"	
+			} } } } } } } } }
+
+			assert_not_nil flash[:notice]
+			assert_match /Operational Event created marking this attempted entry/, flash[:notice]
+			assert_equal study_subject_url( duplicate ), current_url
+		end
+
+		test "should create subject if duplicate subject no match found with #{cu} login" do
+			duplicate = Factory(:complete_waivered_case_study_subject)
+			login_as send(cu)
+			visit new_waivered_path
+
+			subject = Factory.build(:complete_waivered_case_study_subject)	#	build, but DON'T save
+			#	by using the attributes from this built subject, we test the factory and use its sequencing
+
+			select "male", 	#	text NOT the value
+				:from => "study_subject[sex]"
+			fill_in "study_subject[patient_attributes][hospital_no]",
+				:with => duplicate.hospital_no						#	should trigger duplicate found
+			select subject.organization.to_s,
+				:from => "study_subject[patient_attributes][organization_id]"
+			fill_in "study_subject[patient_attributes][admit_date]",
+				:with => subject.admit_date.strftime("%m/%d/%Y")
+			select "AML", 
+				:from => "study_subject[patient_attributes][diagnosis_id]"
+			fill_in "study_subject[pii_attributes][dob]",
+				:with => subject.dob.strftime("%m/%d/%Y")
+
+			assert_difference('PhoneNumber.count',0) {
+			assert_difference('Addressing.count',0) {
+			assert_difference('Address.count',0) {
+			assert_difference('Pii.count',0) {
+			assert_difference('Patient.count',0) {
+			assert_difference('Identifier.count',0) {
+			assert_difference('Enrollment.count',0) {
+			assert_difference('StudySubject.count',0) {
+				#	click_button(value)
+				click_button "Submit"	
+			} } } } } } } }
+
+			#	is not redirected, is rendered, therefore it is just a path, not a full url
+			assert_equal waivered_path, current_url
+			assert_not_nil flash[:error]	#	Possible Duplicate(s) Found.
+			assert_match /Possible Duplicate\(s\) Found/, flash[:error]
+
+			assert_difference('PhoneNumber.count',0) {
+			assert_difference('Addressing.count',0) {
+			assert_difference('Address.count',0) {
+			assert_difference('Pii.count',2) {
+			assert_difference('Patient.count',1) {
+			assert_difference('Identifier.count',2) {
+			assert_difference('Enrollment.count',2) {
+			assert_difference('StudySubject.count',2) {
+				#	click_button(value)
+				click_button "No Match"	
+			} } } } } } } }
+
+#			assert_not_nil flash[:notice]
+#			assert_match /Operational Event created marking this attempted entry/, flash[:notice]
+			assert_equal study_subject_url( assigns(:study_subject) ), current_url
 		end
 
 		test "should get new waivered raf form and submit with #{cu} login" do
@@ -13,8 +121,8 @@ pending	#	TODO	complete_waivered_case_study_subject
 
 			visit new_waivered_path
 
-#	attributes = Factory.attributes_for(:complete_nonwaivered_case_study_subject)
-#	then try to use these attributes in form?
+			subject = Factory.build(:complete_waivered_case_study_subject)	#	build, but DON'T save
+			#	by using the attributes from this built subject, we test the factory and use its sequencing
 
 			#	select(option_text, options = {})	
 			#	selects on the inner content of the option tag, NOT the option tag's value.
@@ -22,15 +130,19 @@ pending	#	TODO	complete_waivered_case_study_subject
 			select "male", 
 				:from => "study_subject[sex]"
 			fill_in "study_subject[patient_attributes][hospital_no]",
-				:with => "9999999999999999999999999"				#	This will NEED to be unique
-			select Hospital.waivered.first.organization.to_s,
+				:with => subject.hospital_no
+#				:with => "9999999999999999999999999"				#	This will NEED to be unique
+#			select Hospital.waivered.first.organization.to_s,
+			select subject.organization.to_s,
 				:from => "study_subject[patient_attributes][organization_id]"
 			fill_in "study_subject[patient_attributes][admit_date]",
-				:with => "1/31/1973"
+				:with => subject.admit_date.strftime("%m/%d/%Y")
+#				:with => "1/31/1973"
 			select "AML", 
 				:from => "study_subject[patient_attributes][diagnosis_id]"
 			fill_in "study_subject[pii_attributes][dob]",
-				:with => "12/5/1971"
+				:with => subject.dob.strftime("%m/%d/%Y")
+#				:with => "12/5/1971"
 
 			assert_difference('PhoneNumber.count',0) {
 			assert_difference('Address.count',0) {
@@ -43,6 +155,8 @@ pending	#	TODO	complete_waivered_case_study_subject
 				#	click_button(value)
 				click_button "Submit"	
 			} } } } } } } }
+			assert_nil flash[:error]
+			assert_equal study_subject_url( assigns(:study_subject) ), current_url		#	i would prefer a current_path, but doesn't exist
 		end
 
 	end
