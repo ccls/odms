997db28be2c7d89de8d1f6c8a8afd860be2714e1
Jake <github@jakewendt.com>
2011-11-15 13:29:07 -0800

    Not redirecting on patients#show for non case subjects.[#111]

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -9,20 +9,35 @@ class PatientsController < ApplicationController
 	before_filter :may_destroy_patients_required,
 		:only => :destroy
 
-#	before_filter :valid_hx_study_subject_id_required
 	before_filter :valid_study_subject_id_required
 	before_filter :valid_patient_required,
-		:only => [:show,:edit,:update,:destroy]
+		:only => [:edit,:update,:destroy]
+#		:only => [:show,:edit,:update,:destroy]
 	before_filter :case_study_subject_required,
 		:only => [:new,:create]
 	before_filter :no_patient_required,
 		:only => [:new,:create]
 	
-#	def show
-##	won't actually make it here if not case due to before filter
-##	it will get kicked to :new, then get kicked out
-#		render :action => 'not_case' unless @study_subject.is_case?
-#	end
+#
+#	Attempting to show patient for non-case subject results in
+#		a double redirect.  
+#			valid patient required -> new_patient
+#		then
+#			case study_subject required -> study_subject
+#
+#	This results in a flash message hanging around a bit long.
+#	Need to change this all anyway.
+#
+
+	def show
+		if !@study_subject.is_case?
+			render :action => 'not_case' 
+		elsif( ( @patient = @study_subject.patient ).nil? )
+			access_denied("Valid patient required!",
+				new_study_subject_patient_path(@study_subject))
+#			redirect_to new_study_subject_patient_path(@study_subject)
+		end
+	end
 
 	def new
 		flash.discard	#	dump the Patient required from 'show' redirect
@@ -41,12 +56,6 @@ class PatientsController < ApplicationController
 		render :action => 'new'
 	end
 
-##Editors can modify the current address and designated it as being an invalid address. If they designated the address as invalid, the why_invalid text field is mandatory.
-##
-##By default, there is no value for whether an address is valid.
-##
-##As part of study_subject tracing, we will have the ability to designated whether an address we have has been verified by some means. When an address is verified, the user will set is_verified to true. They will then be required to provide a value for the how_verified field and the system will capture both their user_id in verified_by and the current date in verified_on fields.
-#	
 	def update
 		@patient.update_attributes!(params[:patient])
 		flash[:notice] = "Patient updated"
--- test/functional/patients_controller_test.rb
+++ test/functional/patients_controller_test.rb
@@ -13,27 +13,16 @@ class PatientsControllerTest < ActionController::TestCase
 	assert_no_route(:put,:update)
 	assert_no_route(:delete,:destroy)
 
-#	Not shallow routes so can't use
-#	ASSERT_ACCESS_OPTIONS = {
-#		:model => 'Patient',
-#		:actions => [:edit,:update],
-#		:attributes_for_create => :factory_attributes,
-#		:method_for_create => :create_patient
-#	}
 	def factory_attributes(options={})
 		Factory.attributes_for(:patient,options)
 	end
 
-#	assert_access_with_login({ 
-#		:logins => [:superuser,:admin,:editor] })
-#	assert_no_access_with_login({ 
-#		:logins => [:interviewer,:reader,:active_user] })
-#	assert_no_access_without_login
+	#	All nested routes, so common class-level assertions won't work
 
 	site_editors.each do |cu|
 
 		test "should show patient with #{cu} login" do
-			patient = create_patient_with_study_subject
+			patient = create_patient
 			login_as send(cu)
 			get :show, :study_subject_id => patient.study_subject.id
 			assert assigns(:study_subject)
@@ -49,7 +38,7 @@ class PatientsControllerTest < ActionController::TestCase
 			assert_redirected_to study_subjects_path
 		end
 
-		test "should NOT show patient for patientless study_subject " <<
+		test "should NOT show patient for patientless case study_subject " <<
 				"and #{cu} login" do
 			study_subject = create_case_study_subject
 			assert_nil study_subject.patient
@@ -61,9 +50,25 @@ class PatientsControllerTest < ActionController::TestCase
 			assert_redirected_to new_study_subject_patient_path(study_subject)
 		end
 
+		test "should NOT show patient for control subject with #{cu} login" do
+			study_subject = create_control_study_subject
+			login_as send(cu)
+			get :show, :study_subject_id => study_subject.id
+			assert_nil flash[:error]
+			assert_template 'not_case'
+		end
+
+		test "should NOT show patient for mother subject with #{cu} login" do
+			study_subject = create_mother_study_subject
+			login_as send(cu)
+			get :show, :study_subject_id => study_subject.id
+			assert_nil flash[:error]
+			assert_template 'not_case'
+		end
+
 		test "should NOT get new patient with #{cu} login " <<
 				"for study_subject with patient" do
-			patient = create_patient_with_study_subject
+			patient = create_patient
 			login_as send(cu)
 			get :new, :study_subject_id => patient.study_subject.id
 			assert assigns(:study_subject)
@@ -155,7 +160,7 @@ class PatientsControllerTest < ActionController::TestCase
 		end
 
 		test "should edit patient with #{cu} login" do
-			patient = create_patient_with_study_subject
+			patient = create_patient
 			login_as send(cu)
 			get :edit, :study_subject_id => patient.study_subject.id
 			assert assigns(:patient)
@@ -172,7 +177,7 @@ class PatientsControllerTest < ActionController::TestCase
 		end
 
 		test "should update patient with #{cu} login" do
-			patient = create_patient_with_study_subject
+			patient = create_patient
 			login_as send(cu)
 			put :update, :study_subject_id => patient.study_subject.id,
 				:patient => factory_attributes
@@ -193,7 +198,7 @@ class PatientsControllerTest < ActionController::TestCase
 
 		test "should NOT update patient with #{cu} " <<
 				"login when update fails" do
-			patient = create_patient_with_study_subject(:updated_at => Chronic.parse('yesterday'))
+			patient = create_patient(:updated_at => Chronic.parse('yesterday'))
 			Patient.any_instance.stubs(:create_or_update).returns(false)
 			login_as send(cu)
 			deny_changes("Patient.find(#{patient.id}).updated_at") {
@@ -208,7 +213,7 @@ class PatientsControllerTest < ActionController::TestCase
 
 		test "should NOT update patient with #{cu} " <<
 				"login and invalid patient" do
-			patient = create_patient_with_study_subject(:updated_at => Chronic.parse('yesterday'))
+			patient = create_patient(:updated_at => Chronic.parse('yesterday'))
 			Patient.any_instance.stubs(:valid?).returns(false)
 			login_as send(cu)
 			deny_changes("Patient.find(#{patient.id}).updated_at") {
@@ -223,7 +228,7 @@ class PatientsControllerTest < ActionController::TestCase
 
 		test "should destroy patient with #{cu} login" do
 			login_as send(cu)
-			study_subject = create_patient_with_study_subject.study_subject
+			study_subject = create_patient.study_subject.reload
 			assert_not_nil study_subject.patient
 			assert_difference('Patient.count', -1) do
 				delete :destroy, :study_subject_id => study_subject.id
@@ -238,7 +243,7 @@ class PatientsControllerTest < ActionController::TestCase
 	non_site_editors.each do |cu|
 
 		test "should NOT show patient with #{cu} login" do
-			study_subject = create_case_study_subject
+			study_subject = create_patient.study_subject
 			login_as send(cu)
 			get :show, :study_subject_id => study_subject.id
 			assert_not_nil flash[:error]
@@ -264,7 +269,7 @@ class PatientsControllerTest < ActionController::TestCase
 
 		test "should NOT destroy patient with #{cu} login" do
 			login_as send(cu)
-			study_subject = create_patient_with_study_subject.study_subject
+			study_subject = create_patient.study_subject.reload
 			assert_not_nil study_subject.patient
 			assert_difference('Patient.count', 0) do
 				delete :destroy, :study_subject_id => study_subject.id
@@ -276,7 +281,7 @@ class PatientsControllerTest < ActionController::TestCase
 	end
 
 	test "should NOT show patient without login" do
-		study_subject = create_case_study_subject
+		study_subject = create_patient.study_subject
 		get :show, :study_subject_id => study_subject.id
 		assert_redirected_to_login
 	end
@@ -295,25 +300,13 @@ class PatientsControllerTest < ActionController::TestCase
 	end
 
 	test "should NOT destroy patient without login" do
-		study_subject = create_patient_with_study_subject.study_subject
+		study_subject = create_patient.study_subject.reload
 		assert_not_nil study_subject.patient
 		assert_difference('Patient.count', 0) do
 			delete :destroy, :study_subject_id => study_subject.id
 		end
 		assert_not_nil study_subject.patient
 		assert_redirected_to_login
-	end
-
-protected
-
-	#	TODO clean this up
-	def create_patient_with_study_subject(options={})
-		patient = create_patient({
-			:study_subject => Factory(:case_study_subject)
-		}.merge(options))
-		assert_not_nil patient.id
-		assert_not_nil patient.study_subject
-		patient.reload
 	end
 
 end
