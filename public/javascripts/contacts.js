13e14dd11f4578e19c083e70b8d30616903825e1
Jake <github@jakewendt.com>
2011-11-22 16:19:16 -0800

    Updating style and javascript.

--- public/javascripts/contacts.js
+++ public/javascripts/contacts.js
@@ -3,16 +3,4 @@ jQuery(function(){
 	jQuery('a.toggle_historic_addressings').togglerFor('.addressings .historic');
 	jQuery('a.toggle_historic_phone_numbers').togglerFor('.phone_numbers .historic');
 
-/*
-	jQuery('a.toggle_historic_addressings').click(function(){
-		jQuery('.addressings .historic').toggle()
-		return false;
-	});
-
-	jQuery('a.toggle_historic_phone_numbers').click(function(){
-		jQuery('.phone_numbers .historic').toggle()
-		return false;
-	});
-*/
-
 });
