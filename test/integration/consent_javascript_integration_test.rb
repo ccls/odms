8f8178a38cb79ea0d1f307a7f6a38ea10fa049db
Jake <github@jakewendt.com>
2011-12-03 00:27:59 -0800

    Comment in preparation for test

--- test/integration/consent_javascript_integration_test.rb
+++ test/integration/consent_javascript_integration_test.rb
@@ -6,6 +6,10 @@ class ConsentJavascriptIntegrationTest < ActionController::CapybaraIntegrationTe
 
 #	consent#edit (shouldn't have a consent#new)
 
+
+#	add tests which tests the subject languages checkboxes on kickbacks
+
+
 		test "should not have toggle eligibility criteria for non-case with #{cu} login" do
 			study_subject = Factory(:study_subject)
 			login_as send(cu)
