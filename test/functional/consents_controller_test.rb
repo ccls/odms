c7c0352d494e6d041159b8443ea33614d69badfd
Jake <github@jakewendt.com>
2011-12-03 00:30:17 -0800

    Modified Factory call.  Deving language tests.

--- test/functional/consents_controller_test.rb
+++ test/functional/consents_controller_test.rb
@@ -17,13 +17,47 @@ class ConsentsControllerTest < ActionController::TestCase
 
 	site_editors.each do |cu|
 
-		test "should create subject_languages if given with #{cu} login" do
+		test "should update and create subject_languages if given with #{cu} login" do
+			assert_difference( 'SubjectLanguage.count', 0 ){
+				@study_subject = Factory(:study_subject)
+			}
+			login_as send(cu)
+			put :update, :study_subject_id => @study_subject.id, :study_subject => { :subject_languages_attributes => {
+				:some_random_id => { :language_id => Language.first.id }
+			} }
+#	assert subject has languages
+pending	#	TODO
+			assert_nil     flash[:error]
+			assert_not_nil flash[:notice]
+			assert_redirected_to study_subject_consent_path(assigns(:study_subject))
+		end
+
+		test "should update and destroy subject_languages if given with #{cu} login" do
+			assert_difference( 'SubjectLanguage.count', 1 ){
+				@study_subject = Factory(:study_subject, :subject_languages_attributes => {
+					:some_random_id => { :language_id => Language.first.id }
+			} ) }
+			subject_language = @study_subject.subject_languages.first
+			login_as send(cu)
+#			assert_difference( 'SubjectLanguage.count', -1 ){
+				put :update, :study_subject_id => @study_subject.id, :study_subject => { :subject_languages_attributes => {
+					:some_random_id => { :id => subject_language.id, :_destroy => 1 } } }
+#			}
+#	assert subject has no more languages
+pending	#	TODO
+			assert_nil     flash[:error]
+			assert_not_nil flash[:notice]
+			assert_redirected_to study_subject_consent_path(assigns(:study_subject))
+		end
+
+		test "should NOT update consent if study_subject update fails with #{cu} login" do
 pending	#	TODO
 		end
 
-		test "should destroy subject_languages if given with #{cu} login" do
+		test "should NOT update consent if patient update fails with #{cu} login" do
 pending	#	TODO
 		end
+
 
 		test "should create ccls enrollment on edit if none exists with #{cu} login" do
 			study_subject = Factory(:study_subject)
@@ -58,8 +92,9 @@ pending	#	TODO
 #	These multiple models will need to be wrapped in a transaction.
 #
 
-		test "should get edit consent with #{cu} login" do
-			study_subject = Factory(:enrollment).study_subject
+		test "should edit consent with #{cu} login" do
+#			study_subject = Factory(:enrollment).study_subject
+			study_subject = Factory(:study_subject)
 			login_as send(cu)
 			get :edit, :study_subject_id => study_subject.id
 			assert_not_nil assigns(:study_subject)
@@ -68,15 +103,43 @@ pending	#	TODO
 			assert_template 'edit'
 		end
 
-		test "should NOT get edit consent with invalid study_subject_id #{cu} login" do
+		test "should have eligibility criteria on case edit consent with #{cu} login" do
+			study_subject = Factory(:case_study_subject)
+			login_as send(cu)
+			get :edit, :study_subject_id => study_subject.id
+			assert_response :success
+			assert_template 'edit'
+			assert_select "div.eligibility_criteria", :count => 1
+		end
+
+		test "should NOT have eligibility criteria on control edit consent with #{cu} login" do
+			study_subject = Factory(:control_study_subject)
+			login_as send(cu)
+			get :edit, :study_subject_id => study_subject.id
+			assert_response :success
+			assert_template 'edit'
+			assert_select "div.eligibility_criteria", :count => 0
+		end
+
+		test "should NOT have eligibility criteria on mother edit consent with #{cu} login" do
+			study_subject = Factory(:mother_study_subject)
+			login_as send(cu)
+			get :edit, :study_subject_id => study_subject.id
+			assert_response :success
+			assert_template 'edit'
+			assert_select "div.eligibility_criteria", :count => 0
+		end
+
+		test "should NOT edit consent with invalid study_subject_id #{cu} login" do
 			login_as send(cu)
 			get :edit, :study_subject_id => 0
 			assert_not_nil flash[:error]
 			assert_redirected_to study_subjects_path
 		end
 
-		test "should put update consent with #{cu} login" do
-			study_subject = Factory(:enrollment).study_subject
+		test "should update consent with #{cu} login" do
+#			study_subject = Factory(:enrollment).study_subject
+			study_subject = Factory(:study_subject)
 			login_as send(cu)
 			put :update, :study_subject_id => study_subject.id,
 				:enrollment => Factory.attributes_for(:enrollment)
@@ -85,15 +148,16 @@ pending	#	TODO
 			assert_redirected_to study_subject_consent_path(assigns(:study_subject))
 		end
 
-		test "should NOT put update consent with invalid study_subject_id and #{cu} login" do
+		test "should NOT update consent with invalid study_subject_id and #{cu} login" do
 			login_as send(cu)
 			put :update, :study_subject_id => 0
 			assert_not_nil flash[:error]
 			assert_redirected_to study_subjects_path
 		end
 
-		test "should NOT put update consent with #{cu} login and invalid enrollment" do
-			study_subject = Factory(:enrollment).study_subject
+		test "should NOT update consent with #{cu} login and invalid enrollment" do
+#			study_subject = Factory(:enrollment).study_subject
+			study_subject = Factory(:study_subject)
 			login_as send(cu)
 			Enrollment.any_instance.stubs(:valid?).returns(false)
 			put :update, :study_subject_id => study_subject.id,
@@ -104,8 +168,9 @@ pending	#	TODO
 			assert_template 'edit'
 		end
 
-		test "should NOT put update consent with #{cu} login and save fails" do
-			study_subject = Factory(:enrollment).study_subject
+		test "should NOT update consent with #{cu} login and save fails" do
+#			study_subject = Factory(:enrollment).study_subject
+			study_subject = Factory(:study_subject)
 			login_as send(cu)
 			Enrollment.any_instance.stubs(:create_or_update).returns(false)
 			put :update, :study_subject_id => study_subject.id,
@@ -120,16 +185,18 @@ pending	#	TODO
 
 	non_site_editors.each do |cu|
 
-		test "should NOT get edit consent with #{cu} login" do
-			study_subject = Factory(:enrollment).study_subject
+		test "should NOT edit consent with #{cu} login" do
+#			study_subject = Factory(:enrollment).study_subject
+			study_subject = Factory(:study_subject)
 			login_as send(cu)
 			get :edit, :study_subject_id => study_subject.id
 			assert_not_nil flash[:error]
 			assert_redirected_to root_path
 		end
 
-		test "should NOT put update consent with #{cu} login" do
-			study_subject = Factory(:enrollment).study_subject
+		test "should NOT update consent with #{cu} login" do
+#			study_subject = Factory(:enrollment).study_subject
+			study_subject = Factory(:study_subject)
 			login_as send(cu)
 			put :update, :study_subject_id => study_subject.id
 			assert_not_nil flash[:error]
@@ -141,7 +208,8 @@ pending	#	TODO
 	site_readers.each do |cu|
 
 		test "should get consents with #{cu} login" do
-			study_subject = Factory(:enrollment).study_subject
+#			study_subject = Factory(:enrollment).study_subject
+			study_subject = Factory(:study_subject)
 			login_as send(cu)
 			get :show, :study_subject_id => study_subject.id
 			assert assigns(:study_subject)
@@ -160,23 +228,11 @@ pending	#	TODO
 
 		test "should NOT get consents for mother with #{cu} login" do
 			login_as send(cu)
-#			study_subject = mother = nil
-#			assert_difference('Enrollment.count',2) {
-#			assert_difference('StudySubject.count',2) {
-#				study_subject = Factory(:identifier).reload.study_subject
-#				assert_not_nil study_subject.identifier
-#				#	subject currently needs identifier to use create_mother
-#				mother = study_subject.create_mother
-#			} }
-#			assert_not_nil mother
-#			ccls_enrollment = mother.enrollments.find_by_project_id(Project['ccls'].id)
-#			assert_not_nil ccls_enrollment
 			mother = Factory(:mother_study_subject)
 			get :show, :study_subject_id => mother.id
-#			assert_not_nil flash[:error]
-#			assert_match /This is a mother subject. .*data is only collected for child subjects. Please go to the record for the subject's child for details/, flash[:error]
 			assert_nil flash[:error]
-			assert_match /data is only collected for child subjects. Please go to the record for the subject's child for details/, @response.body
+			assert_match /data is only collected for child subjects. Please go to the record for the subject's child for details/, 
+				@response.body
 			assert_response :success
 			assert_template 'show_mother'
 			assert_nil assigns(:enrollment)
@@ -187,7 +243,8 @@ pending	#	TODO
 	non_site_readers.each do |cu|
 
 		test "should NOT get consents with #{cu} login" do
-			study_subject = Factory(:enrollment).study_subject
+#			study_subject = Factory(:enrollment).study_subject
+			study_subject = Factory(:study_subject)
 			login_as send(cu)
 			get :show, :study_subject_id => study_subject.id
 			assert_not_nil flash[:error]
@@ -197,19 +254,22 @@ pending	#	TODO
 	end
 
 	test "should NOT get consents without login" do
-		study_subject = Factory(:enrollment).study_subject
+#		study_subject = Factory(:enrollment).study_subject
+		study_subject = Factory(:study_subject)
 		get :show, :study_subject_id => study_subject.id
 		assert_redirected_to_login
 	end
 
 	test "should NOT get edit consent without login" do
-		study_subject = Factory(:enrollment).study_subject
+#		study_subject = Factory(:enrollment).study_subject
+		study_subject = Factory(:study_subject)
 		get :edit, :study_subject_id => study_subject.id
 		assert_redirected_to_login
 	end
 
-	test "should NOT put update consent without login" do
-		study_subject = Factory(:enrollment).study_subject
+	test "should NOT update consent without login" do
+#		study_subject = Factory(:enrollment).study_subject
+		study_subject = Factory(:study_subject)
 		put :update, :study_subject_id => study_subject.id
 		assert_redirected_to_login
 	end
