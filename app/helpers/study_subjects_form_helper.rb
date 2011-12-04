b7da4b921bf0a53a0609010937d565011c85df43
Jake <github@jakewendt.com>
2011-12-04 00:10:53 -0800

    Deving subject language tests.

--- app/helpers/study_subjects_form_helper.rb
+++ app/helpers/study_subjects_form_helper.rb
@@ -27,55 +27,10 @@ module StudySubjectsFormHelper
 			s << "<div class='#{classes.join(' ')}'>"
 			self.fields_for( :subject_languages, sl ) do |sl_fields|
 				s << "<div id='other_language'>" if( l.is_other? )
-#
-#				label = sl_fields.object.language.key.dup.capitalize
-#				label << (( l.is_other? ) ? ' (not eligible)' : ' (eligible)')
-#
-#				if sl.id.nil?	#	not currently existing subject_language
-#					#	If exists, the hidden id tag is actually immediately put in the html stream!
-#					#	Don't think that it will be a problem, but erks me.
-#					s << sl_fields.check_box( :language_id, {
-#						:checked => selected_language_ids.include?(sl.language_id.to_s),
-#					}, sl.language_id, '' ) << "\n"
-#					s << sl_fields.label( :language_id, label ) << "\n"
-#				else	#	language exists, this is for possible destruction
-#					#	check_box(object_name, method, options = {}, 
-#					#		checked_value = "1", unchecked_value = "0")
-#					#	when checked, I want it to do nothing (0), when unchecked I want destroy (1)
-#					#	Here, I only want existing language_ids
-#					#	Yes, this is very backwards.
-#
-#					#	KEEP ME for finding _destroy!
-#					s << sl_fields.hidden_field( :language_id, :value => sl.language_id )
-#
-##	sl_params ...
-## {"0"=>{"id"=>"1", "language_id"=>"1", "_destroy"=>"0"}, "1"=>{"language_id"=>""}, "2"=>{"language_id"=>"", "other"=>""}}
-#
-#					attrs = sl_params.detect{|p| p[1]['language_id'] == sl.language_id.to_s }
-##					["0", {"id"=>"1", "language_id"=>"1", "_destroy"=>"1"}]
-#
-#					#	only uncheck the checkbox if it was unchecked.
-#					language_checked = ( !attrs.nil? and attrs[1].has_key?('_destroy') ) ?
-#						( attrs[1]['_destroy'] == '0' ) : true
-#					
-#					s << sl_fields.check_box( :_destroy, {
-#						:checked => language_checked
-#					}, 0, 1 ) << "\n"
-#					s << sl_fields.label( :_destroy, label ) << "\n"
-#				end
-#
-#				s << sl_fields.specify_other_language() if l.is_other?
-#				if( l.is_other? )
-#					s << "<div id='specify_other_language'>"
-#					s << sl_fields.label( :other, 'specify:' ) << "\n"
-#					s << sl_fields.text_field( :other, :size => 12 ) << "\n"
-#					s << "</div>"	# id='other_language'>"
-#				end
-
 
 #	I would rather not be picking from the params, but for the moment it seems the only way.
+#	I'd prefer to base it on the existing object, saved or unsaved, but don't know how to do that.
 #	Perhaps once this is working, I can change it up, but I don't foresee that.
-
 
 				if sl.id.nil?	#	not currently existing subject_language
 					s << sl_fields.subject_language_creator(selected_language_ids.include?(sl.language_id.to_s))
@@ -100,23 +55,38 @@ module StudySubjectsFormHelper
 		s << "</div>\n"	#	languages
 		s << "</div><!-- study_subject_languages -->\n"	#	study_subject_languages
 	end
+	alias_method :subject_languages_selector, :subject_languages_select
+
+	def subject_races_select
+	end
+	alias_method :subject_races_selector, :subject_races_select
 
 protected
 
-	def subject_language_creator(checked=false)
-		#	self.object is a subject_language
+	def race_label
+	end
+
+	def subject_race_creator
+	end
+
+	def subject_race_destroyer
+	end
+
+	def language_label
 		label =  self.object.language.key.dup.capitalize
 		label << (( self.object.language.is_other? ) ? ' (not eligible)' : ' (eligible)')
+	end
+
+	def subject_language_creator(checked=false)
+		#	self.object is a subject_language
 		#	If exists, the hidden id tag is actually immediately put in the html stream!
 		#	Don't think that it will be a problem, but erks me.
 		s = self.check_box( :language_id, { :checked => checked }, self.object.language_id, '' ) << "\n"
-		s << self.label( :language_id, label ) << "\n"
+		s << self.label( :language_id, self.language_label ) << "\n"
 	end
 
 	def subject_language_destroyer(checked=true)
 		#	self.object is a subject_language
-		label =  self.object.language.key.dup.capitalize
-		label << (( self.object.language.is_other? ) ? ' (not eligible)' : ' (eligible)')
 		#	KEEP ME for finding _destroy to determine if checked
 		s =  self.hidden_field( :language_id, :value => self.object.language_id )
 		#	check_box(object_name, method, options = {}, 
@@ -125,7 +95,7 @@ protected
 		#	Here, I only want existing language_ids
 		#	Yes, this is very backwards.
 		s << self.check_box( :_destroy, { :checked => checked }, 0, 1 ) << "\n"
-		s << self.label( :_destroy, label ) << "\n"
+		s << self.label( :_destroy, self.language_label ) << "\n"
 	end
 
 	def specify_other_language
