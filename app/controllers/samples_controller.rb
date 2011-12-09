fac74a224b5cb8bb06ee301244f02869edaf1997
Jake <github@jakewendt.com>
2011-12-09 14:42:34 -0800

    Importing sample functionality from homex app.[#137]

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,30 +1,31 @@
 class SamplesController < ApplicationController
 
-	before_filter :may_read_study_subjects_required, 
-		:only => [:dashboard,:find,:followup,:reports,:index]
+#	before_filter :may_read_study_subjects_required, 
+#		:only => [:dashboard,:find,:followup,:reports,:index]
 
-#	before_filter :may_create_samples_required,
-#		:only => [:new,:create]
-#	before_filter :may_read_samples_required,
-#		:only => [:show,:index,:dashboard,:find,:followup,:reports]
-#	before_filter :may_update_samples_required,
-#		:only => [:edit,:update]
-#	before_filter :may_destroy_samples_required,
-#		:only => :destroy
+	before_filter :may_create_samples_required,
+		:only => [:new,:create]
+	before_filter :may_read_samples_required,
+		:only => [:show,:index,:dashboard,:find,:followup,:reports]
+	before_filter :may_update_samples_required,
+		:only => [:edit,:update]
+	before_filter :may_destroy_samples_required,
+		:only => :destroy
 
 	before_filter :valid_study_subject_id_required,
-		:only => [:index]
-#		:except => [:dashboard,:find,:followup,:reports,:edit,:update,:show,:destroy]
-#	before_filter :valid_id_required,
-#		:only => [:show,:edit,:update,:destroy]
+		:except => [:dashboard,:find,:followup,:reports,:edit,:update,:show,:destroy]
+#		:only => [:index]
+	before_filter :valid_id_required,
+		:only => [:show,:edit,:update,:destroy]
 
 	def index
+		@samples = @study_subject.samples
 		render :layout => 'subject'
 	end
 
-end
-__END__
-
+	def show
+		render :layout => 'subject'
+	end
 
 	def new
 		@sample = @study_subject.samples.new
@@ -40,23 +41,21 @@ __END__
 		render :action => 'new', :layout => 'subject'
 	end
 
+	def edit
+		render :layout => 'subject'
+	end
+
 	def update
 		@sample.update_attributes!(params[:sample])
-#		redirect_to sample_study_subject_path(@study_subject)
 		redirect_to sample_path(@sample)
 	rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotSaved
 		flash.now[:error] = "Sample update failed."
 		render :action => 'edit', :layout => 'subject'
 	end
 
-	def index
-		@samples = @study_subject.samples
-		render :layout => 'subject'
-	end
-
 	def destroy
 		@sample.destroy
-		redirect_to sample_study_subjects_path
+		redirect_to study_subject_path(@study_subject)
 	end
 
 protected
@@ -64,13 +63,12 @@ protected
 	def valid_id_required
 		if !params[:id].blank? and Sample.exists?(params[:id])
 			@sample = Sample.find(params[:id])
-#			@study_subject = @sample.study_subject
-#	in dev on brg, above fails so being more explicit, the below works
-			@study_subject = StudySubject.find(@sample.study_subject_id)
+			@study_subject = @sample.study_subject
+##	in dev on brg, above fails so being more explicit, the below works
+#			@study_subject = StudySubject.find(@sample.study_subject_id)
 		else
-			access_denied("Valid sample id required!", 
-				study_subjects_path)
+			access_denied("Valid sample id required!", study_subjects_path)
 		end
 	end
 
-
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -46,8 +46,9 @@ class User < Ccls::User
 	end
 
 	%w(	contacts guides home_page_pics patients
-			response_sets interviews samples
+			response_sets interviews 
 			).each do |resource|
+#samples
 		alias_method "may_create_#{resource}?".to_sym,  :may_edit?
 		alias_method "may_read_#{resource}?".to_sym,    :may_edit?
 		alias_method "may_edit_#{resource}?".to_sym,    :may_edit?
@@ -57,6 +58,7 @@ class User < Ccls::User
 
 	%w(	addressings addresses home_exposures phone_numbers study_subjects
 			enrollments events projects documents notes consents
+			samples
 			).each do |resource|
 		alias_method "may_create_#{resource}?".to_sym,  :may_create?
 		alias_method "may_read_#{resource}?".to_sym,    :may_read?
