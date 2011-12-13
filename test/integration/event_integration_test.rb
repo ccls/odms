974a27be202ffbf2fd416833127f16b21ba6beee
Jake <github@jakewendt.com>
2011-12-13 14:20:00 -0800

    Deving event creation and operational_event_types controller.[#115]

--- test/integration/event_integration_test.rb
+++ test/integration/event_integration_test.rb
@@ -2,6 +2,12 @@ require 'integration_test_helper'
 
 class EventIntegrationTest < ActionController::CapybaraIntegrationTest
 
+#	Will probably need to add OperationalEvent and
+#		OperationalEventType to the shared connection 
+	test "should add test to test ajax calling" do
+pending	#	TODO
+	end
+
 #	site_administrators.each do |cu|
 #
 #	end
