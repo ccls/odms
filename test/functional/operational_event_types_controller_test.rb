30ea474b739410a7beef0eefa67b77a389b8da24
Jake <github@jakewendt.com>
2011-12-13 15:20:38 -0800

    Deving event creation and operational_event_types controller.[#115]

--- test/functional/operational_event_types_controller_test.rb
+++ test/functional/operational_event_types_controller_test.rb
@@ -2,20 +2,33 @@ require 'test_helper'
 
 class OperationalEventTypesControllerTest < ActionController::TestCase
 
+	def setup
+		#	only validates html pages, not json
+		#	These will be partials.
+		#	don't validate this page.  Should be an easier way, but this works.
+		Html::Test::ValidateFilter.any_instance.stubs(:should_validate?).returns(false)
+	end
+
 	test "should get options without category" do
 		get :options
-		puts @response.body
+#		puts @response.body
+#	this will probably change as all will have categories
+#Operational Event Types Controller should get options without category: <option value=":other event - please specify">:other event - please specify</option>
+#		assert_select 'option'
 	end
 
 	test "should get options with invalid category" do
 		get :options, :category => 'IDONOTEXIST'
-		puts @response.body
+		assert @response.body.blank?
 	end
 
 	test "should get options with valid category" do
 		category = OperationalEventType.categories.first
 		get :options, :category => category
-		puts @response.body
+		assert_select 'option'
+#		puts @response.body
+#<option value="ascertainment:A new RAF was sent for an existing patient">ascertainment:A new RAF was sent for an existing patient</option>
+#<option value="ascertainment:New subject created by CCLS">ascertainment:New subject created by CCLS</option>
 	end
 
 end
--- test/functional/zip_codes_controller_test.rb
+++ test/functional/zip_codes_controller_test.rb
@@ -2,34 +2,60 @@ require 'test_helper'
 
 class ZipCodesControllerTest < ActionController::TestCase
 
-	test "should get zip_codes with no q" do
-		pending	#	TODO
-		get :index
-		puts @response.body
+	def setup
+		#	only validates html pages, not json
+		#	don't validate this page.  Should be an easier way, but this works.
+		Html::Test::ValidateFilter.any_instance.stubs(:should_validate?).returns(false)
 	end
 
-	test "should get zip_codes with blank q" do
-		pending	#	TODO
-		get :index, :q => ''
-		puts @response.body
+#	zip_codes in html is full html page
+#    <h1>ZipCodes#index</h1>
+#<p>Find me in app/views/zip_codes/index.html.erb</p>
+#<table><tbody>
+#<tr>
+#<td>NORTHUMBERLAND</td>
+#<td>PA</td>
+#<td>17857</td>
+#<td>Northumberland</td>
+#</tr>
+#</tbody></table>
+
+
+#	test "should get zip_codes with no q" do
+#		pending	#	TODO
+#		get :index
+##		puts @response.body
+#	end
+#
+#	test "should get zip_codes with blank q" do
+#		pending	#	TODO
+#		get :index, :q => ''
+##		puts @response.body
+#	end
+#
+#	test "should get zip_codes with partial q" do
+#		pending	#	TODO
+#		get :index, :q => '178'
+##		puts @response.body
+#	end
+#
+#	test "should get zip_codes with full q" do
+#		pending	#	TODO
+#		get :index, :q => '17857'
+##		puts @response.body
+#	end
+
+	test "should get zip_codes.json with full q" do
+		#	:format MUST be a string and NOT a symbol
+		get :index, :q => '17857', :format => 'json'
+		expected = %{[{"zip_code":{"county_name":"Northumberland","city":"NORTHUMBERLAND","zip_code":"17857","county_id":2144,"state":"PA"}}]}
+		assert_equal expected, @response.body
 	end
 
-	test "should get zip_codes with partial q" do
-		pending	#	TODO
-		get :index, :q => '178'
-		puts @response.body
-	end
-
-	test "should get zip_codes with full q" do
-		pending	#	TODO
-		get :index, :q => '17857'
-		puts @response.body
-	end
-
-	test "should get zip_codes with invalid q" do
-		pending	#	TODO
-		get :index, :q => 'IDONOTEXIST'
-		puts @response.body
-	end
+#	test "should get zip_codes with invalid q" do
+#		pending	#	TODO
+#		get :index, :q => 'IDONOTEXIST'
+##		puts @response.body
+#	end
 
 end
