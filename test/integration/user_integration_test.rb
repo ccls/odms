71177f75df63b67ec815d9f10d6eecd5c836b705
Jake <github@jakewendt.com>
2011-12-14 11:26:00 -0800

    Using visit instead of get.

--- test/integration/user_integration_test.rb
+++ test/integration/user_integration_test.rb
@@ -2,19 +2,15 @@ require 'integration_test_helper'
 
 class UserIntegrationTest < ActionController::WebRatIntegrationTest
 
-#	def setup
-#		@session = open_session # or ActionController::Integration::Session.new
-#		assert !@session.https?
-#		@session.https!
-#		assert @session.https?
-#	end
-
 	all_test_roles.each do |cu|
 
 		test "should get #{cu} info with #{cu} login" do
 			u = send(cu)
 			login_as u
-			get user_path(u), {}, { 'HTTPS' => 'on' }
+			#	get user_path(u), {}, { 'HTTPS' => 'on' }
+			#	get does not use the set headers
+			#	use visit instead
+			visit user_path(u)
 			assert_response :success
 			assert_not_nil assigns(:user)
 			assert_equal u, assigns(:user)
@@ -22,13 +18,11 @@ class UserIntegrationTest < ActionController::WebRatIntegrationTest
 
 		test "should not get #{cu} info if not logged in" do
 			u = send(cu)
-			get user_path(u), {}, { 'HTTPS' => 'on' }
+			#	get user_path(u), {}, { 'HTTPS' => 'on' }
+			#	get does not use the set headers
+			#	use visit instead
+			visit user_path(u)
 			assert_redirected_to_login
-
-#			@session.get user_path(u)
-#			@session.assert_response :redirect
-#			assert_match "https://auth-test.berkeley.edu/cas/login",
-#				@session.response.redirected_to
 		end
 
 	end
