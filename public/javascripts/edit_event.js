3041ae6954954151488093ef35c421555304404d
Jake <github@jakewendt.com>
2011-12-13 16:02:25 -0800

    Deving event creation and operational_event_types controller.[#115]

--- public/javascripts/edit_event.js
+++ public/javascripts/edit_event.js
@@ -2,12 +2,19 @@ jQuery(function(){
 
 	var root = /(ccls|genepi.).berkeley.edu/.test(location.host)?'/odms':''
 
-	jQuery('select#category').change(function(){
-		jQuery.get(root + '/operational_event_types/options.js?category=' + jQuery(this).val(), 
+	$('select#category').change(function(){
+		$.get(root + '/operational_event_types/options.js?category=' + $(this).val(), 
 			function(data){
 				$('select#operational_event_operational_event_type_id').html(data);
 			}
 		);
 	});
+
+/*
+
+	Assuming that I'll need to add some type of trigger for "specify" unless
+	it will always be available.
+
+*/
 
 });
--- test/integration/event_integration_test.rb
+++ test/integration/event_integration_test.rb
@@ -5,8 +5,15 @@ class EventIntegrationTest < ActionController::CapybaraIntegrationTest
 #	Will probably need to add OperationalEvent and
 #		OperationalEventType to the shared connection 
 	test "should add test to test ajax calling" do
-pending	#	TODO
+pending	#	TODO	should change contents of 
 	end
+
+#	$('select#category').change(function(){
+#		$.get(root + '/operational_event_types/options.js?category=' + $(this).val(), 
+#			function(data){
+#				$('select#operational_event_operational_event_type_id').html(data);
+#		});
+#	});
 
 #	site_administrators.each do |cu|
 #
