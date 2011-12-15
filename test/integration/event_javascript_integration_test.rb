12714ce7b67ada2ed19d47f24950d29822e02b56
Jake <github@jakewendt.com>
2011-12-15 15:53:07 -0800

    Added javascript tests for edit event action.[#115]

--- test/integration/event_javascript_integration_test.rb
+++ test/integration/event_javascript_integration_test.rb
@@ -2,47 +2,90 @@ require 'integration_test_helper'
 
 class EventJavascriptIntegrationTest < ActionController::CapybaraIntegrationTest
 
-	def setup
-		super
+	setup :skip_operational_event_type_page_validation
+	def skip_operational_event_type_page_validation
 		OperationalEventTypesController.skip_after_filter :validate_page
 	end
 
 	site_editors.each do |cu|
 
-		test "should get new study_subject event with #{cu} login" do
+		test "should change event types on category change on new event with #{cu} login" do
 			study_subject = Factory(:study_subject)
 			login_as send(cu)
 			page.visit new_study_subject_event_path(study_subject)
 			assert page.has_css?('select#category')
 
 			assert_equal 11, page.all('select#category option').length
-# [nil,ascertainment,compensation,completions,correspondence,enrollments,interviews,operations,other,recruitment,samples]
+			# [nil,ascertainment,compensation,completions,correspondence,
+			#		enrollments,interviews,operations,other,recruitment,samples]
 
 			assert page.has_css?('select#operational_event_operational_event_type_id')
 			assert_equal 1, 
 				page.all('select#operational_event_operational_event_type_id option').length
-			assert page.find('select#operational_event_operational_event_type_id option').text.blank?
+			assert page.find('select#operational_event_operational_event_type_id option'
+				).text.blank?
 
 			select 'operations', :from => 'category'
 
-#			sleep 2	#	give ajax a couple seconds to actually do its thing
-
-#			assert_equal 8, 
-#				page.all('select#operational_event_operational_event_type_id option').length
+			#	now should have some options.
 			#	by doing it this was, capybara 'reloads' the contents before comparison
 			#	apparently 'all' does not do the same thing, and so requires a bit of waiting.
 			assert_equal 8, 
-				page.find('select#operational_event_operational_event_type_id').all('option').length
+				page.find('select#operational_event_operational_event_type_id'
+					).all('option').length
+			page.find('select#operational_event_operational_event_type_id'
+				).all('option').each { |option|
+					assert !option.text.blank?
+					assert_match /^operations:/, option.text }
 
-			#	now has some options.  Yay!
-#			page.all('select#operational_event_operational_event_type_id option').each do |option|
-#				assert !option.text.blank?
-#			end
-			page.find('select#operational_event_operational_event_type_id').all('option').each do |option|
-				assert !option.text.blank?
-			end
-#			assert_equal 8, 
-#				page.all('select#operational_event_operational_event_type_id option').length
+			select 'samples', :from => 'category'
+
+			assert_equal 3, 
+				page.find('select#operational_event_operational_event_type_id'
+					).all('option').length
+			page.find('select#operational_event_operational_event_type_id'
+				).all('option').each { |option|
+					assert !option.text.blank?
+					assert_match /^samples:/, option.text }
+		end
+
+		test "should change event types on category change on edit event with #{cu} login" do
+			study_subject = Factory(:study_subject)
+			ccls_enrollment = study_subject.enrollments.find_by_project_id(
+				Project['ccls'].id)
+			event = ccls_enrollment.operational_events.first	#	new subject
+			login_as send(cu)
+			page.visit edit_event_path(event)
+			assert page.has_css?('select#category')
+
+			assert_equal 11, page.all('select#category option').length
+			# [nil,ascertainment,compensation,completions,correspondence,
+			#		enrollments,interviews,operations,other,recruitment,samples]
+
+			assert_equal 'ascertainment',
+				page.find('select#category option[selected=selected]').text
+
+			assert page.has_css?('select#operational_event_operational_event_type_id')
+
+			assert_equal 2, 
+				page.find('select#operational_event_operational_event_type_id'
+					).all('option').length
+			page.find('select#operational_event_operational_event_type_id'
+				).all('option').each { |option|
+					assert !option.text.blank?
+					assert_match /^ascertainment:/, option.text }
+
+			select 'operations', :from => 'category'
+			#	now should have some different options.
+			#	by doing it this was, capybara 'reloads' the contents before comparison
+			#	apparently 'all' does not do the same thing, and so requires a bit of waiting.
+			assert_equal 8, 
+				page.find('select#operational_event_operational_event_type_id'
+					).all('option').length
+			page.find('select#operational_event_operational_event_type_id'
+				).all('option').each { |option|
+					assert !option.text.blank?
+					assert_match /^operations:/, option.text }
 		end
 
 	end
