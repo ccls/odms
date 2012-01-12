07a6eab1946a88ece2b13c0206bd1a14768f24b1
Jake <github@jakewendt.com>
2012-01-12 10:28:13 -0800

    Cleanup

--- app/controllers/related_subjects_controller.rb
+++ app/controllers/related_subjects_controller.rb
@@ -2,53 +2,20 @@ class RelatedSubjectsController < ApplicationController
 
 	layout 'subject'
 
+	#	may create?  Perhaps, just may read.
 	before_filter :may_create_study_subjects_required
 	before_filter :valid_id_required, :only => :show
-#	before_filter :case_study_subject_required, :only => :show
 
-#	def index
-#		unless params[:patid].blank?
-#			@study_subject = StudySubject.find_case_by_patid(params[:patid])
-#		end
-#		render :layout => 'application'
-#	end
-
+	#	using the index action seems like it would be more
+	#	appropriate, but then it would need to be a nested
+	#	controller ... study_subjects/:id/related_subjects
+	#	Nicer perhaps, but no different functionally.
 	def show
-#		if !@study_subject.is_case?
-#			render :action => 'not_case' 
-#		else
-			#@control_subjects  = @study_subject.controls
-			@matching_subjects = @study_subject.matching
-			#@family_subjects   = @study_subject.family
-			@rejected_controls = @study_subject.rejected_controls
-#		end
+		#@control_subjects  = @study_subject.controls
+		@matching_subjects = @study_subject.matching
+		#@family_subjects   = @study_subject.family
+		@rejected_controls = @study_subject.rejected_controls
 	end
-
-#	def new
-#		@hospitals = Hospital.all
-#		render :layout => 'application'
-#	end
-#
-#	def create
-#		#	use find_by_id rather than just find so that 
-#		#	if returns nil rather than raise an error if not found
-#		if params[:hospital_id] and
-#			( hospital = Hospital.find_by_id( params[:hospital_id] ) )
-#
-#			new_params = { :study_subject => {
-#				:patient_attributes => {
-#					:organization_id => hospital.organization_id
-#			} } }
-#			if hospital.has_irb_waiver
-#				redirect_to new_waivered_path(new_params)
-#			else
-#				redirect_to new_nonwaivered_path(new_params)
-#			end
-#		else
-#			flash[:error] = 'Please select an organization'
-#			redirect_to new_case_path
-#		end
-#	end
 
 protected
 
@@ -59,12 +26,5 @@ protected
 			access_denied("Valid study_subject id required!", cases_path)
 		end
 	end
-
-#	def case_study_subject_required
-#		unless @study_subject.is_case?
-#			access_denied("Valid case study_subject required!", #	cases_path)
-#				study_subject_path(@study_subject) )
-#		end
-#	end
 
 end
