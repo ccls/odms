69a6e049895addc9fc5b066795e382e88652bb61
Jake <github@jakewendt.com>
2012-01-17 12:16:42 -0800

    Adding test graphs to subject dashboard.[#145]

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,2 +1,22 @@
 class ChartsController < ApplicationController
+	def enrollments
+		@enrollments = Enrollment.all(
+			:select => 'project_id, count(*) as count', 
+			:group => 'project_id')
+	end
+	def vital_statuses
+		@study_subjects = StudySubject.all(
+			:select => 'vital_status_id, count(*) as count', 
+			:group => 'vital_status_id')
+	end
+	def subject_types
+		@study_subjects = StudySubject.all(
+			:select => 'subject_type_id, count(*) as count', 
+			:group => 'subject_type_id')
+	end
+	def case_control_types
+		@identifiers = Identifier.all(
+			:select => 'case_control_type, count(*) as count', 
+			:group => 'case_control_type')
+	end
 end
