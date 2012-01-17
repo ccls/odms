5840ae883683e3f326b23a3396c9bdaedf7b6e6d
Jake <github@jakewendt.com>
2012-01-17 13:25:32 -0800

    Added some pie charts and set flash wmode so menus are above the graphs.[#145]

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -9,12 +9,27 @@ class ChartsController < ApplicationController
 			:select => 'vital_status_id, count(*) as count', 
 			:group => 'vital_status_id')
 	end
+	def vital_statuses_pie
+		@study_subjects = StudySubject.all(
+			:select => 'vital_status_id, count(*) as count', 
+			:group => 'vital_status_id')
+	end
 	def subject_types
 		@study_subjects = StudySubject.all(
 			:select => 'subject_type_id, count(*) as count', 
 			:group => 'subject_type_id')
 	end
+	def subject_types_pie
+		@study_subjects = StudySubject.all(
+			:select => 'subject_type_id, count(*) as count', 
+			:group => 'subject_type_id')
+	end
 	def case_control_types
+		@identifiers = Identifier.all(
+			:select => 'case_control_type, count(*) as count', 
+			:group => 'case_control_type')
+	end
+	def case_control_types_pie
 		@identifiers = Identifier.all(
 			:select => 'case_control_type, count(*) as count', 
 			:group => 'case_control_type')
