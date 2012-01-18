12adcebf520c5180f5b0659fe23174c990aefac6
Jake <github@jakewendt.com>
2012-01-18 13:10:57 -0800

    Meddling with charts.[#145]

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -8,28 +8,32 @@ class ChartsController < ApplicationController
 		@study_subjects = StudySubject.all(
 #			:select => 'vital_status_id, count(*) as count', 
 			:joins => [:vital_status],
-			:select => 'vital_status_id, count(*) as count, vital_statuses.id, vital_statuses.description', 
+#			:select => 'vital_status_id, count(*) as count, vital_statuses.id, vital_statuses.description', 
+			:select => 'vital_status_id, count(*) as count, vital_statuses.*', 
 			:group => 'vital_status_id')
 	end
 	def vital_statuses_pie
 		@study_subjects = StudySubject.all(
 #			:select => 'vital_status_id, count(*) as count', 
 			:joins => [:vital_status],
-			:select => 'vital_status_id, count(*) as count, vital_statuses.id, vital_statuses.description', 
+#			:select => 'vital_status_id, count(*) as count, vital_statuses.id, vital_statuses.description', 
+			:select => 'vital_status_id, count(*) as count, vital_statuses.*', 
 			:group => 'vital_status_id')
 	end
 	def subject_types
 		@study_subjects = StudySubject.all(
 #			:select => 'subject_type_id, count(*) as count',
 			:joins => [:subject_type],
-			:select => 'subject_type_id, count(*) as count, subject_types.id, subject_types.description', 
+#			:select => 'subject_type_id, count(*) as count, subject_types.id, subject_types.description', 
+			:select => 'subject_type_id, count(*) as count, subject_types.*', 
 			:group => 'subject_type_id')
 	end
 	def subject_types_pie
 		@study_subjects = StudySubject.all(
 #			:select => 'subject_type_id, count(*) as count', 
 			:joins => [:subject_type],
-			:select => 'subject_type_id, count(*) as count, subject_types.id, subject_types.description', 
+#			:select => 'subject_type_id, count(*) as count, subject_types.id, subject_types.description', 
+			:select => 'subject_type_id, count(*) as count, subject_types.*', 
 			:group => 'subject_type_id')
 	end
 	def case_control_types
