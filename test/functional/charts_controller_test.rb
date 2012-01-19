efdedd8eb6445871c507443b6fbb9104e087a5d2
Jake <github@jakewendt.com>
2012-01-19 14:41:45 -0800

    Added simple chart tests for 100% coverage.

--- test/functional/charts_controller_test.rb
+++ test/functional/charts_controller_test.rb
@@ -1,4 +1,27 @@
 require 'test_helper'
 
 class ChartsControllerTest < ActionController::TestCase
+
+	ChartsController.action_methods.each do |action|
+
+		all_test_roles.each do |cu|
+
+			test "should get #{action} with #{cu} login" do
+				login_as send(cu)
+				get action, :format => 'json'
+				assert_response :success
+			end
+
+		end
+
+		test "should NOT get #{action} without login" do
+			get action, :format => 'json'
+			#	json requests do not redirect, apparently
+			assert @response.body.blank?
+			assert_response :unauthorized
+#			assert_redirected_to_login
+		end
+
+	end
+
 end
