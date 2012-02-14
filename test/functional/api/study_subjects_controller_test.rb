4891264607e276fe4f6a15a826955c8bff7f3f21
Jake <github@jakewendt.com>
2012-02-14 13:42:36 -0800

    More api tests.

--- test/functional/api/study_subjects_controller_test.rb
+++ test/functional/api/study_subjects_controller_test.rb
@@ -11,8 +11,31 @@ class Api::StudySubjectsControllerTest < ActionController::TestCase
 		assert assigns(:study_subjects).include?(study_subject)
 	end
 
+	test "should not get study_subjects with http" do
+		turn_https_off
+		set_credentials
+		get :index
+		assert_response 302
+		assert_response :redirect	#	to https
+	end
+
+	test "should not get study_subjects without password" do
+		set_credentials('admin')
+		get :index
+		assert_response 401
+		assert_response :unauthorized
+		#	action_controller/status_codes.rb
+	end
+
+	test "should not get study_subjects with bad credentials" do
+		set_credentials('foo','bar')
+		get :index
+		assert_response 401
+		assert_response :unauthorized
+		#	action_controller/status_codes.rb
+	end
+
 	test "should not get study_subjects without credentials" do
-		study_subject = create_study_subject
 		get :index
 		assert_response 401
 		assert_response :unauthorized
@@ -35,11 +58,20 @@ class Api::StudySubjectsControllerTest < ActionController::TestCase
 
 protected 
 
-	def set_credentials
-		config = YAML::load(ERB.new(IO.read("#{RAILS_ROOT}/config/api.yml")).result)
+	def set_credentials(*args)
+		username, password = if args.empty?
+			username_and_password
+		else
+			[ args[0], args[1] ]
+		end
 		@request.env['HTTP_AUTHORIZATION'
 			] = ActionController::HttpAuthentication::Basic.encode_credentials(
-				config[:user],config[:password])
+				username, password)
+	end
+
+	def username_and_password
+		config = YAML::load(ERB.new(IO.read("#{RAILS_ROOT}/config/api.yml")).result)
+		[ config[:user], config[:password] ]
 	end
 
 end
