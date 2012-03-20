6033be97be3c342ce5ef0b211b30d49537c74a2e
Jake <github@jakewendt.com>
2012-03-19 20:39:41 -0700

    Corrected test.

--- test/unit/helpers/subject_language_select_helper_test.rb
+++ test/unit/helpers/subject_language_select_helper_test.rb
@@ -75,18 +75,13 @@ class SubjectLanguageSelectHelperTest < ActionView::TestCase
 		@study_subject = Factory(:study_subject, :subject_languages_attributes => {
 			'0' => { :language_id => Language['english'].id } } )
 		subject_language_id = @study_subject.subject_language_ids.first	#	this can vary so cannot assume that it will be 1
-#
-#	TODO where is subject_language_id?  Does this still work? No, it doesn't.  Now it does.
-#
-pending
 		output_buffer = form_for(@study_subject,:url => '/'){|f| 
 			f.subject_languages_select([Language['english'],Language['spanish'],Language['other']]) }
-puts output_buffer
 		expected = %{<form accept-charset="UTF-8" action="/" class="edit_study_subject" id="edit_study_subject_#{@study_subject.id}" method="post"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /><input name="_method" type="hidden" value="put" /></div><div id='study_subject_languages'><div class='languages_label'>Language of parent or caretaker:</div>
 <div id='languages'>
 <div class='subject_language destroyer'><input id="study_subject_subject_languages_attributes_0_language_id" name="study_subject[subject_languages_attributes][0][language_id]" type="hidden" value="1" /><input name="study_subject[subject_languages_attributes][0][_destroy]" type="hidden" value="1" /><input checked="checked" id="english__destroy" name="study_subject[subject_languages_attributes][0][_destroy]" type="checkbox" value="0" />
 <label for="english__destroy">English (eligible)</label>
-</div>
+<input id="study_subject_subject_languages_attributes_0_id" name="study_subject[subject_languages_attributes][0][id]" type="hidden" value="#{subject_language_id}" /></div>
 <div class='subject_language creator'><input name="study_subject[subject_languages_attributes][1][language_id]" type="hidden" value="" /><input id="spanish_language_id" name="study_subject[subject_languages_attributes][1][language_id]" type="checkbox" value="2" />
 <label for="spanish_language_id">Spanish (eligible)</label>
 </div>
@@ -105,16 +100,8 @@ puts output_buffer
 		@study_subject = Factory(:study_subject, :subject_languages_attributes => {
 			'0' => { :language_id => Language['other'].id, :other_language => 'redneck' } } )
 		subject_language_id = @study_subject.subject_language_ids.first	#	this can vary so cannot assume that it will be 1
-
 		output_buffer = form_for(@study_subject,:url => '/'){|f| 
 			f.subject_languages_select([Language['english'],Language['spanish'],Language['other']]) }
-puts output_buffer
-
-#
-#	TODO where'd the subject_language_id go??
-#	TODO where is subject_language_id?  Does this still work?
-#
-pending
 		expected = %{<form accept-charset="UTF-8" action="/" class="edit_study_subject" id="edit_study_subject_#{@study_subject.id}" method="post"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /><input name="_method" type="hidden" value="put" /></div><div id='study_subject_languages'><div class='languages_label'>Language of parent or caretaker:</div>
 <div id='languages'>
 <div class='subject_language creator'><input name="study_subject[subject_languages_attributes][0][language_id]" type="hidden" value="" /><input id="english_language_id" name="study_subject[subject_languages_attributes][0][language_id]" type="checkbox" value="1" />
@@ -127,7 +114,7 @@ pending
 <label for="other__destroy">Other (not eligible)</label>
 <div id='specify_other_language'><label for="other_other_language">specify:</label>
 <input id="other_other_language" name="study_subject[subject_languages_attributes][2][other_language]" size="12" type="text" value="redneck" />
-</div></div></div>
+</div></div><input id="study_subject_subject_languages_attributes_2_id" name="study_subject[subject_languages_attributes][2][id]" type="hidden" value="#{subject_language_id}" /></div>
 </div>
 </div><!-- study_subject_languages -->
 </form>}
