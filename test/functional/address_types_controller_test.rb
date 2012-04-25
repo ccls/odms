183c768766b2c940f1cc37f26f83e7be6d90ca22
Jake <github@jakewendt.com>
2012-04-25 12:37:31 -0700

    Corrected functional tests to allow only admins.[#24]

--- test/functional/address_types_controller_test.rb
+++ test/functional/address_types_controller_test.rb
@@ -13,8 +13,8 @@ class AddressTypesControllerTest < ActionController::TestCase
 		Factory.attributes_for(:address_type,options)
 	end
 
-	assert_access_with_login({    :logins => site_editors })
-	assert_no_access_with_login({ :logins => non_site_editors })
+	assert_access_with_login({    :logins => site_administrators })
+	assert_no_access_with_login({ :logins => non_site_administrators })
 	assert_no_access_without_login
 
 end
--- test/functional/follow_up_types_controller_test.rb
+++ test/functional/follow_up_types_controller_test.rb
@@ -13,8 +13,8 @@ class FollowUpTypesControllerTest < ActionController::TestCase
 		Factory.attributes_for(:follow_up_type,options)
 	end
 
-	assert_access_with_login({    :logins => site_editors })
-	assert_no_access_with_login({ :logins => non_site_editors })
+	assert_access_with_login({    :logins => site_administrators })
+	assert_no_access_with_login({ :logins => non_site_administrators })
 	assert_no_access_without_login
 
 end
