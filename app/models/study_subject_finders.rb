9257c549e1f8899a8f1a16db4ad373f3b9ccc2b4
Jake <github@jakewendt.com>
2012-06-25 10:27:00 -0700

    Added check for if child before searching for child.

--- app/models/study_subject_finders.rb
+++ app/models/study_subject_finders.rb
@@ -54,9 +54,24 @@ base.class_eval do
 #		if is_mother? && !familyid.blank?
 #			StudySubject.children.with_subjectid(familyid).not_id(id 
 #				).includes(:subject_type).first
-	#	Find the case or control subject with matching familyid except self.
+
+#
+#	this is kinda excessive.  Check if self is child first
+#	and then if not do this search
+#
+#	#	Find the case or control subject with matching familyid except self.
+#	def child
+#		if !familyid.blank?
+#			StudySubject.children.with_subjectid(familyid).includes(:subject_type).first
+#		else
+#			nil
+#		end
+#	end
+
 	def child
-		if !familyid.blank?
+		if is_case? or is_control?
+			self
+		elsif !familyid.blank?
 			StudySubject.children.with_subjectid(familyid).includes(:subject_type).first
 		else
 			nil
