32ec3d14e090f97787fa8c302c502f1ed68d9add
Jake <github@jakewendt.com>
2012-06-25 12:43:07 -0700

    Added Sample Transfer.[#305]

--- app/models/sample_transfer.rb
+++ app/models/sample_transfer.rb
@@ -2,8 +2,8 @@ class SampleTransfer < ActiveRecord::Base
 #  attr_accessible :destination_org_id, :notes, :sample_id, :sent_on, :source_org_id, :status
 
 	belongs_to :sample
-	belongs_to :source_org_id,      :class_name => "Organization"
-	belongs_to :destination_org_id, :class_name => "Organization"
+	belongs_to :source_org,      :class_name => "Organization"
+	belongs_to :destination_org, :class_name => "Organization"
 
 #	validates_presence_of :sample_id
 #	validates_presence_of :sample, :if => :sample_id
