f059589a82a041107fba648e3c0a30cd37220186
Jake <github@jakewendt.com>
2012-07-27 12:30:15 -0700

    Don't send email in development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,16 @@ Odms::Application.configure do
 	config.consider_all_requests_local			 = true
 	config.action_controller.perform_caching = false
 
+
+
+	# Tell Action Mailer not to deliver emails to the real world.
+	# The :test delivery method accumulates sent emails in the
+	# ActionMailer::Base.deliveries array.
+	config.action_mailer.delivery_method = :test
+
+
+
+
 	# Don't care if the mailer can't send
 	config.action_mailer.raise_delivery_errors = false
 
