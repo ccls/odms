e598952a380b0fc834119d71845080faa5971adf
Jake <github@jakewendt.com>
2012-07-30 13:54:44 -0700

    Deving dynamic column selection for sunspot.

--- app/helpers/sunspot_helper.rb
+++ app/helpers/sunspot_helper.rb
@@ -85,9 +85,37 @@ module SunspotHelper
 		s << "</ul>\n".html_safe
 	end
 
+	def available_columns
+		%w( id case_icf_master_id mother_icf_master_id icf_master_id 
+			subject_type vital_status case_control_type reference_date
+			sex dob died_on phase birth_year 
+mother_first_name
+mother_maiden_name
+mother_last_name
+father_first_name
+father_last_name
+first_name
+middle_name
+maiden_name
+last_name
+do_not_contact
+father_ssn
+mother_ssn
+patid
+languages
+)
+	end
+
+#
+#	language_names
+#
+
 	def default_columns
-		%w( id subject_type vital_status case_control_type reference_date
-		 sex dob died_on phase birth_year )
+		%w( id case_icf_master_id mother_icf_master_id icf_master_id 
+			subject_type vital_status sex dob 
+first_name
+last_name
+)
 	end
 
 	def columns
@@ -108,6 +136,12 @@ module SunspotHelper
 	def column_content(subject,column)
 		case column.to_s
 			when 'dob','died_on','reference_date' then subject.send(column).try(:strftime,'%m/%d/%Y')
+			when 'languages' then subject.languages.collect(&:key).join(',')
+			when 'icf_master_id' then subject.icf_master_id_to_s
+			when 'case_icf_master_id' then subject.case_subject.try(:icf_master_id_to_s)||'[No Case Subject]'
+			when 'mother_icf_master_id' then subject.mother.try(:icf_master_id_to_s)||'[No Mother Subject]'
+			when 'father_ssn' then subject.birth_data.order('created_at DESC').collect(&:father_ssn).compact.first
+			when 'mother_ssn' then subject.birth_data.order('created_at DESC').collect(&:mother_ssn).compact.first
 			else ( subject.respond_to?(column) ? subject.try(column) : nil )
 		end
 	end
