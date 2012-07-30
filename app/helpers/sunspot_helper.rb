3adb8169f587caf1c178c5cd25add839a74809a5
Jake <github@jakewendt.com>
2012-07-30 16:28:57 -0700

    Deving dynamic column selection for sunspot.

--- app/helpers/sunspot_helper.rb
+++ app/helpers/sunspot_helper.rb
@@ -89,33 +89,31 @@ module SunspotHelper
 		%w( id case_icf_master_id mother_icf_master_id icf_master_id 
 			subject_type vital_status case_control_type reference_date
 			sex dob died_on phase birth_year 
-mother_first_name
-mother_maiden_name
-mother_last_name
-father_first_name
-father_last_name
-first_name
-middle_name
-maiden_name
-last_name
-do_not_contact
-father_ssn
-mother_ssn
-patid
-languages
-)
+			mother_first_name mother_maiden_name mother_last_name
+			father_first_name father_last_name 
+			first_name middle_name maiden_name last_name
+			do_not_contact
+			father_ssn mother_ssn
+			patid languages subjectid
+			hospital_no admit_date )
 	end
 
-#
-#	language_names
-#
+#Eligible
+#Consented
+#Interview assigned
+#Interview date
+#Abstract date completed
+#Abstract date requested
+#Abstract date received
+#Abstractor
+#Abstract checker
+#Sample type/sub-type
+#Date samples received
 
 	def default_columns
 		%w( id case_icf_master_id mother_icf_master_id icf_master_id 
 			subject_type vital_status sex dob 
-first_name
-last_name
-)
+			first_name last_name)
 	end
 
 	def columns
@@ -135,13 +133,20 @@ last_name
 
 	def column_content(subject,column)
 		case column.to_s
-			when 'dob','died_on','reference_date' then subject.send(column).try(:strftime,'%m/%d/%Y')
-			when 'languages' then subject.languages.collect(&:key).join(',')
-			when 'icf_master_id' then subject.icf_master_id_to_s
-			when 'case_icf_master_id' then subject.case_subject.try(:icf_master_id_to_s)||'[No Case Subject]'
-			when 'mother_icf_master_id' then subject.mother.try(:icf_master_id_to_s)||'[No Mother Subject]'
-			when 'father_ssn' then subject.birth_data.order('created_at DESC').collect(&:father_ssn).compact.first
-			when 'mother_ssn' then subject.birth_data.order('created_at DESC').collect(&:mother_ssn).compact.first
+			when 'dob','died_on','reference_date','admit_date'
+				subject.send(column).try(:strftime,'%m/%d/%Y')
+			when 'languages' 
+				subject.languages.collect(&:key).join(',')
+			when 'icf_master_id' 
+				subject.icf_master_id_to_s
+			when 'case_icf_master_id' 
+				subject.case_subject.try(:icf_master_id_to_s)||'[No Case Subject]'
+			when 'mother_icf_master_id' 
+				subject.mother.try(:icf_master_id_to_s)||'[No Mother Subject]'
+			when 'father_ssn' 
+				subject.birth_data.order('created_at DESC').collect(&:father_ssn).compact.first
+			when 'mother_ssn' 
+				subject.birth_data.order('created_at DESC').collect(&:mother_ssn).compact.first
 			else ( subject.respond_to?(column) ? subject.try(column) : nil )
 		end
 	end
