6d8ca7c18695ffdf8a19df75082be6d0cf9a21df
Jake <github@jakewendt.com>
2012-08-02 14:03:46 -0700

    Adding some sunspot tests.

--- test/unit/study_subject_sunspot_test.rb
+++ test/unit/study_subject_sunspot_test.rb
@@ -1,4 +1,22 @@
 require 'test_helper'
 
 class StudySubjectSunspotTest < ActiveSupport::TestCase
+
+	if StudySubject.respond_to?(:solr_search)
+
+		test "should search" do
+			Sunspot.remove_all!					#	isn't always necessary
+			StudySubject.solr_reindex
+			assert StudySubject.search.hits.empty?
+			Factory(:study_subject)
+			StudySubject.solr_reindex
+			assert !StudySubject.search.hits.empty?
+		end
+
+	else
+#
+#	Sunspot wasn't running when test started
+#
+	end
+
 end
