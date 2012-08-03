567593798e50be7b94deab6e894377d14452920e
Jake <github@jakewendt.com>
2012-08-03 10:45:47 -0700

    Code cleanup

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -183,7 +183,7 @@ class ApplicationHelperTest < ActionView::TestCase
 				study_subject = Factory(:study_subject)
 				response = HTML::Document.new( subject_side_menu(study_subject) ).root
 				assert_select response, 'div#sidemenu' do
-					assert_select 'a', :count => 15
+					assert_select 'a', :count => 17
 					assert_select 'a.current', :count => 1
 					assert_select 'a.current[href=?]', send(v,study_subject)
 				end
@@ -222,7 +222,7 @@ class ApplicationHelperTest < ActionView::TestCase
 				study_subject = Factory(:study_subject)
 				response = HTML::Document.new( subject_side_menu(study_subject) ).root
 				assert_select response, 'div#sidemenu' do
-					assert_select 'a', :count => 11
+					assert_select 'a', :count => 13
 					if %w( birth_records interviews documents notes ).include?(k)
 						#	not shown to non-admins
 						assert_select 'a.current', :count => 0
@@ -249,7 +249,7 @@ class ApplicationHelperTest < ActionView::TestCase
 			study_subject = Factory(:study_subject)
 			response = HTML::Document.new( subject_side_menu(study_subject) ).root
 			assert_select response, 'div#sidemenu' do
-				assert_select 'a', :count => 11
+				assert_select 'a', :count => 13
 				assert_select 'a.current', :count => 0
 			end
 		end
@@ -273,7 +273,7 @@ class ApplicationHelperTest < ActionView::TestCase
 			study_subject = Factory(:study_subject)
 			response = HTML::Document.new( subject_side_menu(study_subject) ).root
 			assert_select response, 'div#sidemenu' do
-				assert_select 'a', :count => 11
+				assert_select 'a', :count => 13
 				if %w( birth_records interviews documents notes ).include?(k)
 					#	not shown to non-admins
 					assert_select 'a.current', :count => 0
