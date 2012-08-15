f5e2f74e5749d1dee53c4bb86ba1f30f1ef89b1d
Jake <github@jakewendt.com>
2012-08-15 09:59:50 -0700

    Added permissions for screening data stuff.[#377][#378]

--- app/controllers/screening_data_controller.rb
+++ app/controllers/screening_data_controller.rb
@@ -1,2 +1,15 @@
 class ScreeningDataController < ApplicationController
+
+	before_filter :may_create_screening_data_required,
+		:only => [:new,:create,:parse]
+	before_filter :may_read_screening_data_required,
+		:only => [:show,:index]
+	before_filter :may_update_screening_data_required,
+		:only => [:edit,:update]
+	before_filter :may_destroy_screening_data_required,
+		:only => :destroy
+
+	before_filter :valid_id_required, 
+		:only => [:show,:edit,:update,:destroy,:parse]
+
 end
--- app/controllers/screening_datum_updates_controller.rb
+++ app/controllers/screening_datum_updates_controller.rb
@@ -1,2 +1,16 @@
 class ScreeningDatumUpdatesController < ApplicationController
+
+	before_filter :may_create_screening_datum_updates_required,
+		:only => [:new,:create,:parse]
+	before_filter :may_read_screening_datum_updates_required,
+		:only => [:show,:index]
+	before_filter :may_update_screening_datum_updates_required,
+		:only => [:edit,:update]
+	before_filter :may_destroy_screening_datum_updates_required,
+		:only => :destroy
+
+	before_filter :valid_id_required, 
+		:only => [:show,:edit,:update,:destroy,:parse]
+
+
 end
