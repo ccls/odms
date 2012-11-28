5df7178c6d32d588d59fc6bd7cb140981d6960a0
Jake <github@jakewendt.com>
2012-11-28 15:39:05 -0800

    Cleaning up RAF javascript.

--- app/assets/javascripts/raf.js
+++ app/assets/javascripts/raf.js
@@ -2,25 +2,9 @@ jQuery(function(){
 
 	var root = /(ccls|genepi.).berkeley.edu/.test(location.host)?'/odms':''
 
-	jQuery('#study_subject_patient_attributes_raf_zip').change(function(){
+	jQuery('input.zip_field').change(function(){
 		jQuery.get(root + '/zip_codes.json?q=' + jQuery(this).val(), function(data){
 			if(data.length == 1) {
-/*
-				update_city_state_county(data[0].zip_code);
-	in rails 3, doesn't include first key so trying without
-*/
-				update_address_info(data[0]);
-			}
-		});
-	});
-
-	jQuery('#study_subject_addressings_attributes_0_address_attributes_zip').change(function(){
-		jQuery.get(root + '/zip_codes.json?q=' + jQuery(this).val(), function(data){
-			if(data.length == 1) {
-/*
-				update_address_info(data[0].zip_code);
-	in rails 3, doesn't include first key so trying without
-*/
 				update_address_info(data[0]);
 			}
 		});
@@ -49,15 +33,12 @@ jQuery(function(){
 	});
 
 
-
-
 	// use class so that this works for both county fields.
-	var county_fields = jQuery('.county_field');
-	var state_field = jQuery(
-		'#study_subject_addressings_attributes_0_address_attributes_state');
+	var county_fields = jQuery('input.county_field');
+	var state_fields = jQuery('.csz select.state');
 	// this will dump the value on page reload
 	county_fields.autocomplete({ source : california_counties });
-	state_field.change(function(){
+	state_fields.change(function(){
 		if( $(this).val() == 'CA' ){
 			county_fields.autocomplete('option', 'disabled', false );
 		} else {
@@ -77,28 +58,25 @@ var update_address_info = function(zip_code) {
 	[{"zip_code":{"county_name":"Schenectady","city":"SCHENECTADY","zip_code":"12345","state":"NY"}}]
 */
 /* only copy in the values if the target is empty */
-	var address_zip = jQuery('#study_subject_addressings_attributes_0_address_attributes_zip');
-	if( address_zip && !address_zip.val() ){
-		address_zip.val(zip_code.zip_code);
-	}
-	var address_county = jQuery('#study_subject_addressings_attributes_0_address_attributes_county');
-	if( address_county && !address_county.val() ){
-		address_county.val(zip_code.county_name);
-	}
-	var address_city = jQuery('#study_subject_addressings_attributes_0_address_attributes_city');
-	if( address_city && !address_city.val() ){
-		address_city.val(zip_code.city);
-	}
-	var address_state = jQuery('#study_subject_addressings_attributes_0_address_attributes_state');
-	if( address_state && !address_state.val() ){
-		address_state.val(zip_code.state);
-	}
-	var raf_zip = jQuery('#study_subject_patient_attributes_raf_zip');
-	if( raf_zip && !raf_zip.val() ){
-		raf_zip.val(zip_code.zip_code);
-	}
-	var raf_county = jQuery('#study_subject_patient_attributes_raf_county');
-	if( raf_county && !raf_county.val() ){
-		raf_county.val(zip_code.county_name);
-	}
+/* zip_field and county_field for both the zip and county and raf_zip and raf_county*/
+	jQuery('input.zip_field').each( function(){
+		if( !$(this).val() ){
+			$(this).val(zip_code.zip_code);
+		}
+	});
+	jQuery('input.county_field').each(function(){
+		if( !$(this).val() ){
+			$(this).val(zip_code.county_name);
+		}
+	});
+	jQuery('.csz input.city').each(function(){
+		if( !$(this).val() ){
+			$(this).val(zip_code.city);
+		}
+	});
+	jQuery('.csz select.state').each(function(){
+		if( !$(this).val() ){
+			$(this).val(zip_code.state);
+		}
+	});
 }
