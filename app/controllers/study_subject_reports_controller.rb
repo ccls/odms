8e71243076e24f8b84fdc74992c85d0e1e2de512
Jake <github@jakewendt.com>
2012-12-19 11:22:56 -0800

    Removing old case assignment.[#410]

--- app/controllers/study_subject_reports_controller.rb
+++ app/controllers/study_subject_reports_controller.rb
@@ -1,7 +1,5 @@
 class StudySubjectReportsController < ApplicationController
 
-#	INSERT BEFORE FILTERS!
-
 	before_filter :may_edit_required
 
 	def control_assignment
@@ -25,32 +23,6 @@ class StudySubjectReportsController < ApplicationController
 			format.csv { 
 				headers["Content-Disposition"] = "attachment; " <<
 					"filename=newcontrols_#{Time.now.strftime('%m%d%Y')}.csv"
-			}
-		end
-	end
-
-	def case_assignment
-		@study_subjects = StudySubject.cases
-			.where( :phase => 5 )
-			.order('created_at DESC')
-			.joins(:enrollments)
-			.merge(Enrollment.eligible.consented.not_assigned_for_interview.where(:project_id => Project['ccls'].id))
-#http://railscasts.com/episodes/215-advanced-queries-in-rails-3?view=asciicast
-
-#	SELECT `study_subjects`.* FROM `study_subjects` INNER JOIN `subject_types` ON `subject_types`.`id` = `study_subjects`.`subject_type_id` INNER JOIN `enrollments` ON `enrollments`.`study_subject_id` = `study_subjects`.`id` WHERE `subject_types`.`key` = 'Case' AND `study_subjects`.`phase` = 5 AND `enrollments`.`is_eligible` = 1 AND `enrollments`.`consented` = 1 AND `enrollments`.`assigned_for_interview_at` IS NULL AND `enrollments`.`project_id` = 10 ORDER BY created_at DESC
-
-		#
-		#	The only reason to have this block is to change the name of the file.
-		#	By default, it would just be manifest.csv everytime.
-		#	If this is actually desired, remove the entire respond_to block.
-		#
-		#	If removed, one of the test assertions will fails as the 
-		#	Content-Disposition will not be set.
-		#
-		respond_to do |format|
-			format.csv { 
-				headers["Content-Disposition"] = "attachment; " <<
-					"filename=newcases_#{Time.now.strftime('%m%d%Y')}.csv"
 			}
 		end
 	end
--- test/functional/study_subject_reports_controller_test.rb
+++ test/functional/study_subject_reports_controller_test.rb
@@ -22,29 +22,6 @@ class StudySubjectReportsControllerTest < ActionController::TestCase
 			assert_equal subject, assigns(:study_subjects).first
 		end
 
-		test "should get case_assignment with #{cu} login" do
-			login_as send(cu)
-			subject = Factory(:case_study_subject)
-subject.enrollments.where(:project_id => Project['ccls'].id).first.update_attributes({
-	:is_eligible => YNDK[:yes],
-	:consented => YNDK[:yes],
-	:consented_on => Date.today
-})
-			assert_equal 5, subject.phase
-			assert_nil subject.enrollments.where(:project_id => Project['ccls'].id).first.assigned_for_interview_at
-			get :case_assignment, :format => :csv
-			assert_response :success
-			assert_template 'case_assignment'
-#	will this be set automatically??? hmm, let's see ... NOPE
-#	must explicitly set this.  Is that necessary? Doesn't seem to be.
-			assert_not_nil @response.headers['Content-Disposition']
-				.match(/attachment; filename=newcases_.*csv/)
-			assert  assigns(:study_subjects)
-			assert !assigns(:study_subjects).empty?
-			assert_equal 1, assigns(:study_subjects).length
-			assert_equal subject, assigns(:study_subjects).first
-		end
-
 	end
 
 	non_site_editors.each do |cu|
@@ -55,21 +32,10 @@ subject.enrollments.where(:project_id => Project['ccls'].id).first.update_attrib
 			assert_redirected_to root_path
 		end
 
-		test "should NOT get case_assignment with #{cu} login" do
-			login_as send(cu)
-			get :case_assignment, :format => :csv
-			assert_redirected_to root_path
-		end
-
 	end
 
 	test "should NOT get control_assignment without login" do
 		get :control_assignment, :format => :csv
-		assert_redirected_to_login
-	end
-
-	test "should NOT get case_assignment without login" do
-		get :case_assignment, :format => :csv
 		assert_redirected_to_login
 	end
 
