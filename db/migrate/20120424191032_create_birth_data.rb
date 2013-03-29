adaa220f70677512e0622b74f7899fb72b2c34d5
Jake <github@jakewendt.com>
2013-03-28 18:55:58 -0700

    Cleaned up some migrations

--- db/migrate/20120424191032_create_birth_data.rb
+++ db/migrate/20120424191032_create_birth_data.rb
@@ -6,7 +6,6 @@ class CreateBirthData < ActiveRecord::Migration
 #			t.string  :master_id	#	case icf_master_id
 			t.string  :masterid	#	case icf_master_id
 			t.boolean :found_in_state_db
-#			t.boolean :FoundInStateDB
 			t.string  :birth_state
 			t.string  :match_confidence
 			t.string  :case_control_flag
@@ -18,18 +17,15 @@ class CreateBirthData < ActiveRecord::Migration
 			t.integer :mother_race_ethn_2
 			t.integer :mother_race_ethn_3
 
-#			t.string  :abnormal_conditions, :length => 2
-			t.string  :abnormal_conditions
+			t.string  :abnormal_conditions	#	shoulda had :limit => 2, not :length => 2
 			t.integer :apgar_1min
 			t.integer :apgar_5min
 			t.integer :apgar_10min
 			t.integer :birth_order
 			t.string  :birth_type
 			t.decimal :birth_weight_gms
-#			t.string  :complications_labor_delivery, :length => 2
-			t.string  :complications_labor_delivery
-#			t.string  :complications_pregnancy, :length => 2
-			t.string  :complications_pregnancy
+			t.string  :complications_labor_delivery	#	shoulda had :limit => 2, not :length => 2
+			t.string  :complications_pregnancy	#	shoulda had :limit => 2, not :length => 2
 			t.string  :county_of_delivery
 			t.integer :daily_cigarette_cnt_1st_tri
 			t.integer :daily_cigarette_cnt_2nd_tri
@@ -40,19 +36,13 @@ class CreateBirthData < ActiveRecord::Migration
 			t.string  :middle_name
 			t.string  :last_name
 
-#			t.string  :father_job_industry, :length => 20
 			t.string  :father_industry
 			t.date    :father_dob
-#			t.string  :father_hispanic_origin_code, :length => 15
-			t.string  :father_hispanic_origin_code
+			t.string  :father_hispanic_origin_code	#	should had :limit => 15, not :length => 15
 			t.string  :father_first_name
 			t.string  :father_middle_name
 			t.string  :father_last_name
-#			t.string  :father_occupation, :length => 20
-			t.string  :father_occupation
-#			t.string  :father_race_ethnicity, :length => 15
-#father_ssn
-#			t.integer :father_years_education
+			t.string  :father_occupation	#	shoulda had :limit => 20, not :length => 20
 			t.integer :father_yrs_educ
 			t.string  :fetal_presentation_at_birth
 			t.boolean :forceps_attempt_unsuccessful
@@ -64,8 +54,7 @@ class CreateBirthData < ActiveRecord::Migration
 			t.integer :live_births_now_living
 			t.string  :local_registrar_district
 			t.string  :local_registrar_no
-#			t.string  :method_of_delivery, :length => 10
-			t.string  :method_of_delivery
+			t.string  :method_of_delivery	#	should had :limit => 10, not :length => 10
 			t.string  :month_prenatal_care_began
 			t.string  :mother_residence_line_1
 			t.string  :mother_residence_city
@@ -83,21 +72,15 @@ class CreateBirthData < ActiveRecord::Migration
 			t.integer :mother_height
 			t.string  :mother_hispanic_origin_code
 			t.string  :mother_industry
-#			t.string  :mother_job_industry		#	NEW
 			t.string  :mother_occupation
-#			t.string  :mother_race_ethnicity
 			t.boolean :mother_received_wic
-#mother_ssn
 			t.integer :mother_weight_pre_pregnancy
-#			t.integer :mother_years_education
 			t.integer :mother_yrs_educ
 			t.integer :ob_gestation_estimate_at_delivery
 			t.integer :prenatal_care_visit_count
 			t.string  :sex
 			t.string  :state_registrar_no
-#			t.integer :termination_count_20_plus_weeks
 			t.integer :term_count_20_plus_weeks
-#			t.integer :termination_count_before_20_weeks
 			t.integer :term_count_pre_20_weeks
 			t.boolean :vacuum_attempt_unsuccessful
 			t.timestamps
--- db/migrate/20120606231826_add_t2k_and_gegl_to_sample_type.rb
+++ db/migrate/20120606231826_add_t2k_and_gegl_to_sample_type.rb
@@ -1,6 +1,6 @@
 class AddT2kAndGeglToSampleType < ActiveRecord::Migration
 	def change
 		add_column :sample_types, :t2k_sample_type_id, :integer
-		add_column :sample_types, :gegl_sample_type_id, :string, :length => 5
+		add_column :sample_types, :gegl_sample_type_id, :string	#	should had :limit => 5, not :length => 5
 	end
 end
