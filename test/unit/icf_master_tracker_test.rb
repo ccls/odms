1eb8fd9c4a474c0d47f67ee94fb7943e5743289d
Jake <github@jakewendt.com>
2013-04-15 16:27:50 -0700

    Deving some tests

--- test/unit/icf_master_tracker_test.rb
+++ test/unit/icf_master_tracker_test.rb
@@ -3,30 +3,69 @@ require 'test_helper'
 class IcfMasterTrackerTest < ActiveSupport::TestCase
 
 	test "should update blank interview_completed_on with cati_complete if not blank" do
+		study_subject = FactoryGirl.create(:study_subject, :icf_master_id => "IDOEXIST")
+		assert_nil study_subject.ccls_enrollment.interview_completed_on
+		icf_master_tracker = IcfMasterTracker.new(:master_id => 'IDOEXIST',:cati_complete => '12/31/2012')
+		assert_equal Date.parse('12/31/2012'),study_subject.ccls_enrollment.interview_completed_on
 pending
 	end
 
 	test "should update different interview_completed_on with cati_complete if not blank" do
+		study_subject = FactoryGirl.create(:study_subject, :icf_master_id => "IDOEXIST")
+		assert_nil study_subject.ccls_enrollment.interview_completed_on
+		study_subject.ccls_enrollment.update_attribute(:interview_completed_on => '12/31/2000')
+		assert_not_nil study_subject.ccls_enrollment.interview_completed_on
+		assert_equal Date.parse('12/31/2000'),study_subject.ccls_enrollment.interview_completed_on
+		icf_master_tracker = IcfMasterTracker.new(:master_id => 'IDOEXIST',:cati_complete => '12/31/2012')
+		assert_equal Date.parse('12/31/2012'),study_subject.ccls_enrollment.interview_completed_on
 pending
 	end
 
 	test "should create operational event with cati_complete if not blank and changes interview_completed_on" do
+		study_subject = FactoryGirl.create(:study_subject, :icf_master_id => "IDOEXIST")
+		assert_nil study_subject.ccls_enrollment.interview_completed_on
+		icf_master_tracker = IcfMasterTracker.new(:master_id => 'IDOEXIST',:cati_complete => '12/31/2012')
+
+		#	could be more specific
+		oes = study_subject.operational_events
+			.where(:operational_event_type_id => OperationalEventType['other'].id)
+		assert_equal 1, oes.length
+		assert_match /interview_completed_on set to/, oes.first.description
+
 pending
 	end
 
 	test "should not create operational event with cati_complete if not blank and same interview_completed_on" do
+		study_subject = FactoryGirl.create(:study_subject, :icf_master_id => "IDOEXIST")
+		assert_nil study_subject.ccls_enrollment.interview_completed_on
+		study_subject.ccls_enrollment.update_attribute(:interview_completed_on => '12/31/2012')
+		assert_not_nil study_subject.ccls_enrollment.interview_completed_on
+		assert_equal Date.parse('12/31/2012'),study_subject.ccls_enrollment.interview_completed_on
+		icf_master_tracker = IcfMasterTracker.new(:master_id => 'IDOEXIST',:cati_complete => '12/31/2012')
+		assert_nil study_subject.operational_events
+			.where(:operational_event_type_id => OperationalEventType['other'].id)
+
 pending
 	end
 
 	test "should do what if cati_complete blank" do
+		study_subject = FactoryGirl.create(:study_subject, :icf_master_id => "IDOEXIST")
+		icf_master_tracker = IcfMasterTracker.new(:master_id => 'IDOEXIST')
 pending
 	end
 
 	test "should do what if master_id not valid" do
+		icf_master_tracker = IcfMasterTracker.new(:master_id => 'IAMBOGUS')
 pending
 	end
 
 	test "should do what if master_id blank" do
+		icf_master_tracker = IcfMasterTracker.new(:master_id => '')
+pending
+	end
+
+	test "should do what if master_id not given" do
+		icf_master_tracker = IcfMasterTracker.new()
 pending
 	end
 
