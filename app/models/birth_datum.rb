15985103e0d9569965569afbaf21537632a51f5d
Jake <github@jakewendt.com>
2013-05-08 15:52:04 -0700

    Still deving birth data import.[#415]

--- app/models/birth_datum.rb
+++ app/models/birth_datum.rb
@@ -4,7 +4,7 @@ class BirthDatum < ActiveRecord::Base
 	attr_protected :study_subject_id, :study_subject
 	has_one :candidate_control
 	has_many :odms_exceptions, :as => :exceptable
-	before_create :verify_data
+	before_create :cleanup_data
 	after_create :post_processing
 
 	alias_attribute :mother_years_educ, :mother_yrs_educ
@@ -18,7 +18,7 @@ class BirthDatum < ActiveRecord::Base
 		['0','control'].include?(case_control_flag)
 	end
 
-	def verify_data
+	def cleanup_data
 		#
 		#	many times the dob is blank.  Assuming that it is then the same as case dob????
 		#
@@ -125,8 +125,139 @@ class BirthDatum < ActiveRecord::Base
 	#
 	#	Separated this out so that can do separately if needed.
 	#
+	#	Doing this very similar to the bc info updating
+	#	
 	def update_study_subject_attributes
-#		return if master_id.blank? and childid.blank? and subjectid.blank?
+		assign_subject unless study_subject
+		return if study_subject.nil?
+		#
+		#	ONLY DO THIS FOR CASE SUBJECTS!
+		#
+		return unless is_case?
+
+		study_subject.operational_events.create(
+			:occurred_at               => DateTime.current,
+			:project_id                => Project['ccls'].id,
+			:operational_event_type_id => OperationalEventType['birthDataReceived'].id )
+
+		#	using "a" as a synonym for "new_attributes" since is a Hash (pointer)
+		a = new_attributes = HWIA.new
+
+		%w( father_first_name father_middle_name father_last_name 
+				mother_first_name mother_middle_name mother_maiden_name
+				first_name middle_name last_name ).each do |field|
+			a[field] = self.send(field).to_s.squish.namerize
+		end
+
+		a[:sex] = self.sex.to_s.squish.upcase
+		a[:dob] = self.dob
+		a[:state_registrar_no] = self.state_registrar_no.to_s.squish.namerize	#	may not want to do this
+
+		new_attributes.each do |k,v|
+			#	NOTE always check if attribute is blank as don't want to delete data
+			study_subject.send("#{k}=",v) unless v.blank?
+		end
+
+		#	gotta save the changes before the subject, otherwise ... poof
+		#	probably not necessary to save them to the bc_info though
+		changes = study_subject.changes
+
+#puts changes.inspect
+
+#
+#	TODO gonna have to remember this but will need to pull it out in rake task DONE
+#
+#		study_subjects.push(study_subject)
+#
+
+		if study_subject.changed?
+
+			#	kinda crued, but just want to remember that this was changed in email
+			#study_subject.instance_variable_set("@bc_info_changed",true) 
+
+			if study_subject.save
+
+				study_subject.operational_events.create(
+					:occurred_at => DateTime.current,
+					:project_id => Project['ccls'].id,
+					:operational_event_type_id => OperationalEventType['birthDataConflict'].id,
+					:description => "Birth Record data changes from #{birth_data_file_name}",
+					:event_notes => "Changes:  #{changes}")
+
+			else
+
+				study_subject.operational_events.create(
+					:occurred_at => DateTime.current,
+					:project_id => Project['ccls'].id,
+					:operational_event_type_id => OperationalEventType['birthDataConflict'].id,
+					:description => "Birth Record data changes from #{birth_data_file_name}",
+					:event_notes => "StudySubject save failed." << study_subject.errors.full_messages.to_sentence)
+
+#					odms_exceptions.create(
+#						:name        => 'birth data update',
+#						:description => "Error updating case study subject. " <<
+#													"Save failed! " <<
+#													study_subject.errors.full_messages.to_sentence) 
+			end	#	if study_subject.save
+
+		end	#	if study_subject.changed?
+
+	end
+
+	#	Returns string containing candidates's first, middle and last name
+	def full_name
+		[first_name, middle_name, last_name].delete_if(&:blank?).join(' ')
+	end
+
+	#	Returns string containing candidates's mother's first, middle and maiden name
+	def mother_full_name
+		[mother_first_name, mother_middle_name, mother_maiden_name].delete_if(&:blank?).join(' ')
+	end
+
+	def create_address_from_attributes
+		return unless study_subject
+		addressing = study_subject.addressings.new(
+			:address_attributes => {
+				:line_1          => mother_residence_line_1.namerize,
+				:city            => mother_residence_city.namerize,
+				:county          => mother_residence_county.decode_county,
+				:country         => 'United States',
+				:state           => mother_residence_state.decode_state_abbrev,
+				:zip             => mother_residence_zip,
+				:address_type_id => AddressType["residence"].id,
+				:data_source_id  => DataSource["birthdata"].id
+			},
+			:data_source_id => DataSource["birthdata"].id,
+			:notes => "Address is mother's residential address found in the CA State Birth Record.")
+
+		unless addressing.save
+			#	Address possibly contained PO Box which is invalid as a residence.
+			#	Try to create as mailing address...
+			addressing.address.address_type = AddressType["mailing"]
+			study_subject.operational_events.create(
+				:occurred_at => DateTime.current,
+				:project_id                => Project['ccls'].id,
+				:operational_event_type_id => OperationalEventType['bc_received'].id,
+				:description => "Insufficient maternal residence information in birth data to "<<
+					"create address record. See subject's Birth Record page for details.\n"<<
+					"#{addressing.errors.full_messages.to_sentence}" ) unless addressing.save
+		end
+		addressing
+	end
+
+	after_save :reindex_study_subject!
+
+protected
+
+	def reindex_study_subject!
+		study_subject.index if study_subject
+	end
+
+end
+
+__END__
+
+	def update_study_subject_attributes
 		assign_subject unless study_subject
 		return if study_subject.nil?
 		#
@@ -287,56 +418,3 @@ class BirthDatum < ActiveRecord::Base
 				:operational_event_type_id => OperationalEventType['birthDataReceived'].id )
 		end	#	if error_count > 0
 	end
-
-	#	Returns string containing candidates's first, middle and last name
-	def full_name
-		[first_name, middle_name, last_name].delete_if(&:blank?).join(' ')
-	end
-
-	#	Returns string containing candidates's mother's first, middle and maiden name
-	def mother_full_name
-		[mother_first_name, mother_middle_name, mother_maiden_name].delete_if(&:blank?).join(' ')
-	end
-
-	def create_address_from_attributes
-		return unless study_subject
-		addressing = study_subject.addressings.new(
-			:address_attributes => {
-				:line_1          => mother_residence_line_1.namerize,
-				:city            => mother_residence_city.namerize,
-				:county          => mother_residence_county.decode_county,
-				:country         => 'United States',
-				:state           => mother_residence_state.decode_state_abbrev,
-				:zip             => mother_residence_zip,
-				:address_type_id => AddressType["residence"].id,
-				:data_source_id  => DataSource["birthdata"].id
-			},
-			:data_source_id => DataSource["birthdata"].id,
-			:notes => "Address is mother's residential address found in the CA State Birth Record.")
-
-		unless addressing.save
-			#	Address possibly contained PO Box which is invalid as a residence.
-			#	Try to create as mailing address...
-			addressing.address.address_type = AddressType["mailing"]
-			study_subject.operational_events.create(
-				:occurred_at => DateTime.current,
-				:project_id                => Project['ccls'].id,
-				:operational_event_type_id => OperationalEventType['bc_received'].id,
-				:description => "Insufficient maternal residence information in birth data to "<<
-					"create address record. See subject's Birth Record page for details.\n"<<
-					"#{addressing.errors.full_messages.to_sentence}" ) unless addressing.save
-		end
-		addressing
-	end
-
-	after_save :reindex_study_subject!
-
-protected
-
-	def reindex_study_subject!
-		study_subject.index if study_subject
-	end
-
-end
-
-__END__
