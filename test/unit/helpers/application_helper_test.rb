22578f2ffd0d61cafe21519b8b05973c461bedc3
Jake <github@jakewendt.com>
2013-05-10 10:52:36 -0700

    More sidemenu test corrections.

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -162,8 +162,6 @@ class ApplicationHelperTest < ActionView::TestCase
 		'samples'                 => 'study_subject_samples_path',
 		'interviews'              => 'study_subject_interviews_path',
 		'events'                  => 'study_subject_events_path',
-#		'documents'               => 'study_subject_documents_path',
-#		'notes'                   => 'study_subject_notes_path',
 		'related_subjects'        => 'study_subject_related_subjects_path',
 		'abstracts'               => 'study_subject_abstracts_path',
 		'study_subject_abstracts' => 'study_subject_abstracts_path',
@@ -182,7 +180,6 @@ class ApplicationHelperTest < ActionView::TestCase
 				study_subject = FactoryGirl.create(:case_study_subject)
 				response = HTML::Document.new( subject_side_menu(study_subject) ).root
 				assert_select response, '#sidemenu' do
-#					assert_select 'a', :count => 18
 					assert_select 'a', :count => 16
 					assert_select 'a.current', :count => 1
 					assert_select 'a.current[href=?]', send(v,study_subject)
@@ -239,6 +236,11 @@ class ApplicationHelperTest < ActionView::TestCase
 			end
 		end
 
+#	20130510 - don't know why this didn't happen before, but
+#		I removed the notes and documents links and changed #sidemenu
+#		from a div to a ul.  Now rather than having 2 less a tags,
+#		there are 2 more, because the first,prev,next,last a tags
+#		are being counted? x-2+4=x+2
 
 
 
@@ -254,8 +256,7 @@ class ApplicationHelperTest < ActionView::TestCase
 				study_subject = FactoryGirl.create(:case_study_subject)
 				response = HTML::Document.new( subject_side_menu(study_subject) ).root
 				assert_select response, '#sidemenu' do
-					assert_select 'a', :count => 12
-#					if %w( birth_records interviews documents notes ).include?(k)
+					assert_select 'a', :count => 14
 					if %w( birth_records interviews ).include?(k)
 						#	not shown to non-admins
 						assert_select 'a.current', :count => 0
@@ -271,8 +272,7 @@ class ApplicationHelperTest < ActionView::TestCase
 				study_subject = FactoryGirl.create(:control_study_subject)
 				response = HTML::Document.new( subject_side_menu(study_subject) ).root
 				assert_select response, '#sidemenu' do
-					assert_select 'a', :count => 9
-#					if %w( birth_records interviews documents notes rafs
+					assert_select 'a', :count => 11
 					if %w( birth_records interviews rafs
 						abstracts study_subject_abstracts abstract/diagnoses patients ).include?(k)
 						#	not shown to non-admins
@@ -289,8 +289,7 @@ class ApplicationHelperTest < ActionView::TestCase
 				study_subject = FactoryGirl.create(:mother_study_subject)
 				response = HTML::Document.new( subject_side_menu(study_subject) ).root
 				assert_select response, '#sidemenu' do
-					assert_select 'a', :count => 9
-#					if %w( birth_records interviews documents notes rafs
+					assert_select 'a', :count => 11
 					if %w( birth_records interviews rafs
 						abstracts study_subject_abstracts abstract/diagnoses patients ).include?(k)
 						#	not shown to non-admins
@@ -306,9 +305,8 @@ class ApplicationHelperTest < ActionView::TestCase
 				self.params = { :controller => k }
 				study_subject = FactoryGirl.create(:study_subject)
 				response = HTML::Document.new( subject_side_menu(study_subject) ).root
-				assert_select response, '#sidemenu' do
-					assert_select 'a', :count => 9
-#					if %w( birth_records interviews documents notes rafs
+				assert_select response, '#sidemenu' do |s|
+					assert_select 'a', :count => 11
 					if %w( birth_records interviews rafs
 						abstracts study_subject_abstracts abstract/diagnoses patients ).include?(k)
 						#	not shown to non-admins
@@ -384,9 +382,7 @@ class ApplicationHelperTest < ActionView::TestCase
 			study_subject = FactoryGirl.create(:case_study_subject)
 			response = HTML::Document.new( subject_side_menu(study_subject) ).root
 			assert_select response, '#sidemenu' do
-#				assert_select 'a', :count => 14
-				assert_select 'a', :count => 12
-#				if %w( birth_records interviews documents notes ).include?(k)
+				assert_select 'a', :count => 14
 				if %w( birth_records interviews ).include?(k)
 					#	not shown to non-admins
 					assert_select 'a.current', :count => 0
