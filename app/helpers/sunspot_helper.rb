efef53108f370a3b597441839af796eccac869f9
Jake <github@jakewendt.com>
2013-05-13 10:16:45 -0700

    Added 'label' to sunspot column for use on menu.

--- app/helpers/sunspot_helper.rb
+++ app/helpers/sunspot_helper.rb
@@ -39,7 +39,9 @@ module SunspotHelper
 				l[0] = [l[0]].pack('H*')
 				l.join(' : ')
 			else
-				facet_label.titleize
+				column = @sunspot_search_class.all_sunspot_columns.detect{|c|c.name == facet_label}
+				column.label || facet_label.titleize
+#				facet_label.titleize
 			end
 			s << link_to("#{facet_label}&nbsp;(#{non_blank_row_count})".html_safe, 'javascript:void()')
 		end
@@ -96,40 +98,6 @@ module SunspotHelper
 		s << "</ul>\n".html_safe
 	end
 
-#	def available_columns
-##		%w( id case_icf_master_id mother_icf_master_id icf_master_id 
-##			subject_type vital_status case_control_type reference_date
-##			sex dob died_on phase birth_year 
-##			mother_first_name mother_maiden_name mother_last_name
-##			father_first_name father_last_name 
-##			first_name middle_name maiden_name last_name
-##			do_not_contact
-##			father_ssn mother_ssn
-##			patid languages subjectid
-##			hospital hospital_no admit_date )
-#		StudySubject.sunspot_columns.sort + #['ccls_consented','ccls_is_eligible'] +
-#			StudySubject.sunspot_dynamic_columns.sort
-#	end
-#
-##Eligible
-##Consented
-##Interview assigned
-##Interview date
-##Abstract date completed
-##Abstract date requested
-##Abstract date received
-##Abstractor
-##Abstract checker
-##Sample type/sub-type
-##Date samples received
-#
-#	#	made class to more easily accessible by outside testing
-#	def self.default_columns
-#		%w( id case_icf_master_id mother_icf_master_id icf_master_id 
-#			subject_type vital_status sex dob 
-#			first_name last_name)
-#	end
-
 	def columns
 		columns ||= if( params[:c].present? )
 			[params[:c]].flatten
@@ -139,10 +107,6 @@ module SunspotHelper
 	end
 
 	def column_header(column)
-#		case column.to_s
-#			when '' then ''
-#			else sort_link(column,:image => false)
-#		end
 		if @sunspot_search_class.sunspot_orderable_columns.include?(column.to_s)
 			sort_link(column,:image => false)
 		else
@@ -150,29 +114,13 @@ module SunspotHelper
 		end
 	end
 
-#
-#	In order to be sortable in a sunspot search,
-#	it must be given to sunspot in the searchable block
-#	If this is done, then, perhaps, these special column contents
-#	aren't needed?
-#
-#	Except for the date formating?
-#		languages as same name as association
-#		races as same name as association
-#		icf_master_id if want the "[no ID assigned]" when blank
-#
-
-#
-#	NEVER use send on a tainted string!
-#
+	#
+	#	NEVER use send on a tainted string!
+	#
 	def column_content(subject,column)
 		case column.to_s
 			when 'dob','died_on','reference_date','admit_date'
 				subject.send(column).try(:strftime,'%m/%d/%Y')
-#			when 'ccls_consented','ccls_is_eligible'
-#				YNDK[subject.enrollments.where(:project_id => Project['ccls'].id).first.try(
-#					column.to_s.gsub(/^ccls_/,''))]
-
 			when 'languages' 
 				subject.subject_languages.collect(&:to_s).join(',')
 			when 'races' 
@@ -180,14 +128,8 @@ module SunspotHelper
 
 			when *@sunspot_search_class.sunspot_columns
 				( subject.respond_to?(column) ? subject.try(column) : nil )
-#			when /^(.*):(is_eligible|consented)$/
-#				YNDK[Enrollment.where(:study_subject_id => subject.id)
-#					.joins(:project)
-#					.where("projects.description = ?",$1).first.try($2)]
-
 
 #			else returns nil I guess
-
 
 		end
 	end
