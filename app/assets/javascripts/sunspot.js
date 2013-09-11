81caf5255d2e25d71868ab89f33eff9374f65e0d
Jake <github@jakewendt.com>
2013-09-11 10:20:26 -0700

    Trying to remove facet column width flicker.

--- app/assets/javascripts/sunspot.js
+++ app/assets/javascripts/sunspot.js
@@ -1,7 +1,10 @@
 jQuery(function(){
 
 	jQuery('div.facet_toggle a').click(function(){
-		jQuery(this).parent().next().toggle(500);
+//		jQuery(this).parent().next().toggle(500);
+//  added 'blind' so doesn't resize stuff and just slides in.
+//	be advised that this effect temporarily wraps the target in a div until done.
+		jQuery(this).parent().next().toggle('blind',500);
 		jQuery(this).prev().toggleClass('ui-icon-triangle-1-e');
 		jQuery(this).prev().toggleClass('ui-icon-triangle-1-s');
 		return false;
