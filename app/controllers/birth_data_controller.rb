249b842f4255ead824386118b2603f0effb0c9c8
Jake <github@jakewendt.com>
2013-10-17 15:47:33 -0700

    Added some sorting and pagination to birth_data index

--- app/controllers/birth_data_controller.rb
+++ app/controllers/birth_data_controller.rb
@@ -13,7 +13,13 @@ class BirthDataController < ApplicationController
 		:only => [:show,:edit,:update,:destroy]
 
 	def index
-		@birth_data = BirthDatum.scoped
+		record_or_recall_sort_order
+		@birth_data = BirthDatum.paginate(
+				:per_page => params[:per_page]||25,
+				:page     => valid_find_page
+			)
+			.order(search_order)
+
 	end
 
 #	def update
@@ -39,6 +45,20 @@ protected
 			@birth_datum = BirthDatum.find(params[:id])
 		else
 			access_denied("Valid id required!", birth_data_path)
+		end
+	end
+
+
+	def search_order
+		if params[:order] and %w( study_subject_id ).include?(params[:order].downcase)
+			order_string = params[:order]
+			dir = case params[:dir].try(:downcase)
+				when 'desc' then 'desc'
+				else 'asc'
+			end
+			[order_string,dir].join(' ')
+		else
+			nil
 		end
 	end
 
