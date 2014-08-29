6f1bfe6e4bd2135dfaaae2e2b0aae73a6ab5f83b
Jake <github@jakewendt.com>
2014-08-29 10:33:22 -0700

    Added pagination and sortability to icf_master_ids

--- app/controllers/icf_master_ids_controller.rb
+++ app/controllers/icf_master_ids_controller.rb
@@ -7,7 +7,12 @@ class IcfMasterIdsController < ApplicationController
 		:only => [:show,:destroy]
 
 	def index
-		@icf_master_ids = IcfMasterId.all
+		record_or_recall_sort_order
+		@icf_master_ids = IcfMasterId.paginate(
+				:per_page => params[:per_page]||25,
+				:page     => valid_find_page
+			)
+			.order(search_order)
 	end
 
 protected
@@ -17,6 +22,19 @@ protected
 			@icf_master_id = IcfMasterId.find(params[:id])
 		else
 			access_denied("Valid id required!", icf_master_ids_path)
+		end
+	end
+
+	def search_order
+		if params[:order] and %w( icf_master_id study_subject_id assigned_on ).include?(params[:order].downcase)
+			order_string = params[:order]
+			dir = case params[:dir].try(:downcase)
+				when 'desc' then 'desc'
+				else 'asc'
+			end
+			[order_string,dir].join(' ')
+		else
+        		nil
 		end
 	end
 
---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,8 @@
+
+#	inner_window is the number of pages next to the current page in the will_paginate bar.
+#	will_paginate's default is 4.
+WillPaginate::ViewHelpers.pagination_options[:inner_window] = 5
+
+#	outer_window is the number of pages next to the 'previous' and 'next' page links in the will_paginate bar.
+#	will_paginate's default is 1.
+WillPaginate::ViewHelpers.pagination_options[:outer_window] = 5
