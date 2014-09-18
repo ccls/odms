f428cb9aee2391fd6c8db225371e65bfe022afbe
Jake <github@jakewendt.com>
2014-09-18 16:26:52 -0700

    Added sorting to abstracts.

--- app/controllers/abstracts_controller.rb
+++ app/controllers/abstracts_controller.rb
@@ -4,17 +4,37 @@ class AbstractsController < ApplicationController
 	before_filter :may_read_abstracts_required
 
 	def index
-		@abstracts = Abstract.order(:study_subject_id)
+		record_or_recall_sort_order
+		@abstracts = Abstract.joins(:study_subject).order(search_order)
 	end
 
 	def merged
-		@abstracts = Abstract.order(:study_subject_id).merged
+		record_or_recall_sort_order
+		@abstracts = Abstract.joins(:study_subject).order(search_order).merged
 		render :action => :index
 	end
 
 	def to_merge
-		@abstracts = Abstract.order(:study_subject_id).mergable
+		record_or_recall_sort_order
+		@abstracts = Abstract.joins(:study_subject).order(search_order).mergable
 		render :action => :index
+	end
+
+protected
+
+	def search_order
+		if params[:order] and
+				%w( id patid entry_1_by_uid entry_2_by_uid ).include?(
+				params[:order].downcase)
+			order_string = params[:order]
+			dir = case params[:dir].try(:downcase)
+				when 'desc' then 'desc'
+				else 'asc'
+			end
+			[order_string,dir].join(' ')
+		else
+			nil
+		end
 	end
 
 end
