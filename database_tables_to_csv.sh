3672741f42a8f7a5b511495f1a78eb09202d3704
Jake <github@jakewendt.com>
2015-06-24 13:36:25 -0700

    Fixed script that I'll never run again.

--- database_tables_to_csv.sh
+++ database_tables_to_csv.sh
@@ -3,6 +3,9 @@
 echo "Table Name, Field, Type, Null, Key, Default, Human Description"
 
 #	$1 is blank as the first character is a |
+#	sadly, one of the types is decimal(8,2) which contains a comma
+#		and therefore MUST be quoted otherwise won't be parsed correctly.
+
 awk '
 	BEGIN { FS="|"; OFS="," }
 	( /^\s*$/ ) {	}
@@ -12,6 +15,6 @@ awk '
 	( /Field/ ){ }
 	( /schema_migrations/ ){ }
 	( ( /^\|/ ) && ( $2 !~ /Field/ ) ){ 
-		print tablename,$2,$3,$4,$5,$6,""
+		print tablename,$2,"\""$3"\"",$4,$5,$6,""
 	}' database_tables | sed 's/ //g'
 
